a67ada304f2d7ea91c5763f017e2bd2b3b29c8cc
Sami Koskimäki <koskomi@gmail.com>
2021-09-25 10:42:30 +0300

    update docs

--- post-process-docs.js
+++ post-process-docs.js
@@ -4,21 +4,13 @@ const path = require('path')
 const DOCS_PATH = path.join(__dirname, 'docs')
 const DOCS_ASSET_PATH = path.join(DOCS_PATH, 'assets')
 const DOCS_IMAGE_PATH = DOCS_ASSET_PATH
-const DOCS_CSS_PATH = DOCS_ASSET_PATH
-const DOCS_MAIN_CSS_FILE_PATH = path.join(DOCS_CSS_PATH, 'style.css')
 
-const THEME_CSS_PATH = path.join(__dirname, 'docs-theme.css')
 const ASSET_PATH = path.join(__dirname, 'assets')
 
 // Files to go through and replace ASSET_URL_BASE with FIXED_ASSET_URL_BASE.
 const ASSET_FIX_FILES = [path.join(__dirname, 'docs', 'index.html')]
 const ASSET_URL_BASE = 'https://github.com/koskimas/kysely/blob/master/assets'
 const FIXED_ASSET_URL_BASE = 'assets'
-
-// Add our own theme stuff to the main css file.
-const css = fs.readFileSync(DOCS_MAIN_CSS_FILE_PATH).toString()
-const themeCss = fs.readFileSync(THEME_CSS_PATH).toString()
-fs.writeFileSync(DOCS_MAIN_CSS_FILE_PATH, `${css}\n${themeCss}`)
 
 // Copy all assets to doc assets.
 for (const assetName of fs.readdirSync(ASSET_PATH)) {
