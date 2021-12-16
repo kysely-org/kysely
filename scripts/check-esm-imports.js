3e33586397ad7e5da6cf9c5b6e559cd3436271bf
Sami Koskimäki <koskomi@gmail.com>
2021-12-16 10:39:28 +0200

    clean up scripts

--- scripts/check-esm-imports.js
+++ scripts/check-esm-imports.js
@@ -1,19 +1,20 @@
+/**
+ * Goes through all source files and makes sure imports end with '.js'.
+ * If they don't, the ESM version will not work. ESM imports must have
+ * the full file name.
+ */
+
 const fs = require('fs')
 const path = require('path')
 
 let errorsFound = false
 
 function checkDir(dir) {
-  const files = fs.readdirSync(dir).filter((it) => it !== '.' && it !== '..')
-
-  for (const file of files) {
-    const filePath = path.join(dir, file)
+  forEachFile(dir, (filePath) => {
     let errorsFoundInFile = false
 
-    if (isDir(filePath)) {
-      checkDir(filePath)
-    } else if (file.endsWith('.ts')) {
-      for (const row of fs.readFileSync(filePath).toString().split('\n')) {
+    if (filePath.endsWith('.ts')) {
+      for (const row of readLines(filePath)) {
         if (isLocalImport(row) && !isDotJsImport(row)) {
           if (!errorsFoundInFile) {
             if (errorsFound) {
@@ -29,11 +30,30 @@ function checkDir(dir) {
         }
       }
     }
+  })
+}
+
+function forEachFile(dir, callback) {
+  const files = fs.readdirSync(dir).filter((it) => it !== '.' && it !== '..')
+
+  for (const file of files) {
+    const filePath = path.join(dir, file)
+
+    if (isDir(filePath)) {
+      forEachFile(filePath, callback)
+    } else {
+      callback(filePath)
+    }
   }
 }
 
 function isDir(file) {
   return fs.lstatSync(file).isDirectory()
+}
+
+function readLines(filePath) {
+  const data = fs.readFileSync(filePath).toString('utf-8')
+  return data.split('\n')
 }
 
 function isLocalImport(row) {
