add64cde85fff3bdb9e42076bba82f55f1bb0cad
Tomasz Martyński <tomek@martynski.pl>
2022-07-13 22:23:48 +0200

    Add premature connection release test

--- test/node/src/test-setup.ts
+++ test/node/src/test-setup.ts
@@ -101,12 +101,15 @@ if (process.env.TEST_TRANSFORMER) {
   PLUGINS.push(createNoopTransformerPlugin())
 }
 
+export const POOL_SIZE = 20
+
 export const DIALECT_CONFIGS = {
   postgres: {
     database: 'kysely_test',
     host: 'localhost',
     user: 'kysely',
     port: 5434,
+    max: POOL_SIZE,
   },
 
   mysql: {
@@ -118,6 +121,8 @@ export const DIALECT_CONFIGS = {
     // Return big numbers as strings just like pg does.
     supportBigNumbers: true,
     bigNumberStrings: true,
+
+    connectionLimit: POOL_SIZE,
   },
 
   sqlite: {
