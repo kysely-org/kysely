893895bbeda92432f2a662ce03f167ff903c6778
Igal Klebanov <igalklebanov@gmail.com>
2022-07-23 19:16:46 +0300

    add ExplainPlugin.

---
+++ src/plugin/explain/explain-plugin.ts
@@ -0,0 +1,36 @@
+import { QueryResult } from '../../driver/database-connection.js'
+import { RootOperationNode } from '../../query-compiler/query-compiler.js'
+import { ExplainFormat } from '../../util/explainable.js'
+import { AnyRawBuilder, UnknownRow } from '../../util/type-utils.js'
+import {
+  KyselyPlugin,
+  PluginTransformQueryArgs,
+  PluginTransformResultArgs,
+} from '../kysely-plugin.js'
+import { ExplainTransformer } from './explain-transformer.js'
+
+export interface ExplainPluginOptions {
+  format?: ExplainFormat
+  raw?: AnyRawBuilder
+}
+
+/**
+ * Plugin that adds `explain` statement before `select`/`insert`/`update`/`replace`/`delete` keywords.
+ */
+export class ExplainPlugin implements KyselyPlugin {
+  readonly #transformer: ExplainTransformer
+
+  constructor(opt: ExplainPluginOptions = {}) {
+    this.#transformer = new ExplainTransformer(opt)
+  }
+
+  transformQuery(args: PluginTransformQueryArgs): RootOperationNode {
+    return this.#transformer.transformNode(args.node)
+  }
+
+  transformResult(
+    args: PluginTransformResultArgs
+  ): Promise<QueryResult<UnknownRow>> {
+    return Promise.resolve(args.result)
+  }
+}
