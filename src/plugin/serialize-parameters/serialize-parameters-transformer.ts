b9f6b28dbb522c2a7d2e097badd1184540f64d7e
Igal Klebanov <igalklebanov@gmail.com>
2022-08-13 00:35:42 +0300

    fix inserts not working with SerializeParametersPlugin.

--- src/plugin/serialize-parameters/serialize-parameters-transformer.ts
+++ src/plugin/serialize-parameters/serialize-parameters-transformer.ts
@@ -1,4 +1,5 @@
 import { OperationNodeTransformer } from '../../operation-node/operation-node-transformer.js'
+import { PrimitiveValueListNode } from '../../operation-node/primitive-value-list-node.js'
 import { ValueNode } from '../../operation-node/value-node.js'
 import { defaultSerializer, Serializer } from './serialize-parameters.js'
 
@@ -8,6 +9,15 @@ export class SerializeParametersTransformer extends OperationNodeTransformer {
   constructor(serializer: Serializer | undefined) {
     super()
     this.#serializer = serializer || defaultSerializer
+  }
+
+  protected override transformPrimitiveValueList(
+    node: PrimitiveValueListNode
+  ): PrimitiveValueListNode {
+    return {
+      ...node,
+      values: node.values.map(this.#serializer),
+    }
   }
 
   protected override transformValue(node: ValueNode): ValueNode {
