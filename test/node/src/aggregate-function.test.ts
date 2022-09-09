cb75770d241af939f31e196c2d6c7a6f2889db43
Igal Klebanov <igalklebanov@gmail.com>
2022-09-09 13:45:35 +0300

    improve/simplify aggregate function tests.

--- test/node/src/aggregate-function.test.ts
+++ test/node/src/aggregate-function.test.ts
@@ -1,10 +1,18 @@
 import {
+  AggregateFunctionBuilder,
+  ExpressionBuilder,
+  StringReference,
+} from '../../../'
+import {
   BUILT_IN_DIALECTS,
+  Database,
   destroyTest,
   initTest,
   TestContext,
   testSql,
 } from './test-setup.js'
+
+const funcNames = ['avg', 'count', 'max', 'min', 'sum'] as const
 
 for (const dialect of BUILT_IN_DIALECTS) {
   describe(`${dialect}: aggregate functions`, () => {
@@ -18,27 +26,37 @@ for (const dialect of BUILT_IN_DIALECTS) {
       await destroyTest(ctx)
     })
 
-    for (const func of ['avg', 'count', 'max', 'min', 'sum'] as const) {
-      describe(func, () => {
-        it(`should execute a query with ${func}(...) in select clause`, async () => {
+    for (const funcName of funcNames) {
+      describe(funcName, () => {
+        let func: ReturnType<typeof getFuncFromTestContext>
+
+        before(() => {
+          func = getFuncFromTestContext(ctx, funcName)
+        })
+
+        it(`should execute a query with ${funcName}(...) in select clause`, async () => {
           const query = ctx.db
             .selectFrom('person')
             .select([
-              (ctx.db.fn[func] as Function)('person.id').as(func),
-              (qb: any) => qb.fn[func]('person.id').as(`another_${func}`),
+              func('person.id').as(funcName),
+              (eb) =>
+                getFuncFromExpressionBuilder(
+                  eb,
+                  funcName
+                )('person.id').as(`another_${funcName}`),
             ])
 
           testSql(query, dialect, {
             postgres: {
-              sql: `select ${func}("person"."id") as "${func}", ${func}("person"."id") as "another_${func}" from "person"`,
+              sql: `select ${funcName}("person"."id") as "${funcName}", ${funcName}("person"."id") as "another_${funcName}" from "person"`,
               parameters: [],
             },
             mysql: {
-              sql: `select ${func}(\`person\`.\`id\`) as \`${func}\`, ${func}(\`person\`.\`id\`) as \`another_${func}\` from \`person\``,
+              sql: `select ${funcName}(\`person\`.\`id\`) as \`${funcName}\`, ${funcName}(\`person\`.\`id\`) as \`another_${funcName}\` from \`person\``,
               parameters: [],
             },
             sqlite: {
-              sql: `select ${func}("person"."id") as "${func}", ${func}("person"."id") as "another_${func}" from "person"`,
+              sql: `select ${funcName}("person"."id") as "${funcName}", ${funcName}("person"."id") as "another_${funcName}" from "person"`,
               parameters: [],
             },
           })
@@ -46,26 +64,28 @@ for (const dialect of BUILT_IN_DIALECTS) {
           await query.execute()
         })
 
-        it(`should execute a query with ${func}(distinct ...) in select clause`, async () => {
+        it(`should execute a query with ${funcName}(distinct ...) in select clause`, async () => {
           const query = ctx.db
             .selectFrom('person')
             .select([
-              (ctx.db.fn[func] as Function)('person.id').distinct().as(func),
-              (qb: any) =>
-                qb.fn[func]('person.id').distinct().as(`another_${func}`),
+              func('person.id').distinct().as(funcName),
+              (eb) =>
+                getFuncFromExpressionBuilder(eb, funcName)('person.id')
+                  .distinct()
+                  .as(`another_${funcName}`),
             ])
 
           testSql(query, dialect, {
             postgres: {
-              sql: `select ${func}(distinct "person"."id") as "${func}", ${func}(distinct "person"."id") as "another_${func}" from "person"`,
+              sql: `select ${funcName}(distinct "person"."id") as "${funcName}", ${funcName}(distinct "person"."id") as "another_${funcName}" from "person"`,
               parameters: [],
             },
             mysql: {
-              sql: `select ${func}(distinct \`person\`.\`id\`) as \`${func}\`, ${func}(distinct \`person\`.\`id\`) as \`another_${func}\` from \`person\``,
+              sql: `select ${funcName}(distinct \`person\`.\`id\`) as \`${funcName}\`, ${funcName}(distinct \`person\`.\`id\`) as \`another_${funcName}\` from \`person\``,
               parameters: [],
             },
             sqlite: {
-              sql: `select ${func}(distinct "person"."id") as "${func}", ${func}(distinct "person"."id") as "another_${func}" from "person"`,
+              sql: `select ${funcName}(distinct "person"."id") as "${funcName}", ${funcName}(distinct "person"."id") as "another_${funcName}" from "person"`,
               parameters: [],
             },
           })
@@ -73,26 +93,28 @@ for (const dialect of BUILT_IN_DIALECTS) {
           await query.execute()
         })
 
-        it(`should execute a query with ${func}(...) over() in select clause`, async () => {
+        it(`should execute a query with ${funcName}(...) over() in select clause`, async () => {
           const query = ctx.db
             .selectFrom('person')
             .select([
-              (ctx.db.fn[func] as Function)('person.id').over().as(func),
-              (qb: any) =>
-                qb.fn[func]('person.id').over().as(`another_${func}`),
+              func('person.id').over().as(funcName),
+              (eb) =>
+                getFuncFromExpressionBuilder(eb, funcName)('person.id')
+                  .over()
+                  .as(`another_${funcName}`),
             ])
 
           testSql(query, dialect, {
             postgres: {
-              sql: `select ${func}("person"."id") over() as "${func}", ${func}("person"."id") over() as "another_${func}" from "person"`,
+              sql: `select ${funcName}("person"."id") over() as "${funcName}", ${funcName}("person"."id") over() as "another_${funcName}" from "person"`,
               parameters: [],
             },
             mysql: {
-              sql: `select ${func}(\`person\`.\`id\`) over() as \`${func}\`, ${func}(\`person\`.\`id\`) over() as \`another_${func}\` from \`person\``,
+              sql: `select ${funcName}(\`person\`.\`id\`) over() as \`${funcName}\`, ${funcName}(\`person\`.\`id\`) over() as \`another_${funcName}\` from \`person\``,
               parameters: [],
             },
             sqlite: {
-              sql: `select ${func}("person"."id") over() as "${func}", ${func}("person"."id") over() as "another_${func}" from "person"`,
+              sql: `select ${funcName}("person"."id") over() as "${funcName}", ${funcName}("person"."id") over() as "another_${funcName}" from "person"`,
               parameters: [],
             },
           })
@@ -100,28 +122,31 @@ for (const dialect of BUILT_IN_DIALECTS) {
           await query.execute()
         })
 
-        it(`should execute a query with ${func}(...) over(partition by ...) in select clause`, async () => {
+        it(`should execute a query with ${funcName}(...) over(partition by ...) in select clause`, async () => {
           const query = ctx.db.selectFrom('person').select([
-            (ctx.db.fn[func] as Function)('person.id')
-              .over((ob: any) => ob.partitionBy(['person.first_name']))
-              .as(func),
-            (qb: any) =>
-              qb.fn[func]('person.id')
-                .over((ob: any) => ob.partitionBy(['person.first_name']))
-                .as(`another_${func}`),
+            func('person.id')
+              .over((ob) => ob.partitionBy(['person.first_name']))
+              .as(funcName),
+            (eb) =>
+              getFuncFromExpressionBuilder(
+                eb,
+                funcName
+              )('person.id')
+                .over((ob) => ob.partitionBy('person.first_name'))
+                .as(`another_${funcName}`),
           ])
 
           testSql(query, dialect, {
             postgres: {
-              sql: `select ${func}("person"."id") over(partition by "person"."first_name") as "${func}", ${func}("person"."id") over(partition by "person"."first_name") as "another_${func}" from "person"`,
+              sql: `select ${funcName}("person"."id") over(partition by "person"."first_name") as "${funcName}", ${funcName}("person"."id") over(partition by "person"."first_name") as "another_${funcName}" from "person"`,
               parameters: [],
             },
             mysql: {
-              sql: `select ${func}(\`person\`.\`id\`) over(partition by \`person\`.\`first_name\`) as \`${func}\`, ${func}(\`person\`.\`id\`) over(partition by \`person\`.\`first_name\`) as \`another_${func}\` from \`person\``,
+              sql: `select ${funcName}(\`person\`.\`id\`) over(partition by \`person\`.\`first_name\`) as \`${funcName}\`, ${funcName}(\`person\`.\`id\`) over(partition by \`person\`.\`first_name\`) as \`another_${funcName}\` from \`person\``,
               parameters: [],
             },
             sqlite: {
-              sql: `select ${func}("person"."id") over(partition by "person"."first_name") as "${func}", ${func}("person"."id") over(partition by "person"."first_name") as "another_${func}" from "person"`,
+              sql: `select ${funcName}("person"."id") over(partition by "person"."first_name") as "${funcName}", ${funcName}("person"."id") over(partition by "person"."first_name") as "another_${funcName}" from "person"`,
               parameters: [],
             },
           })
@@ -129,36 +154,39 @@ for (const dialect of BUILT_IN_DIALECTS) {
           await query.execute()
         })
 
-        it(`should execute a query with ${func}(...) over(order by ...) in select clause`, async () => {
+        it(`should execute a query with ${funcName}(...) over(order by ...) in select clause`, async () => {
           const query = ctx.db.selectFrom('person').select([
-            (ctx.db.fn[func] as Function)('person.id')
-              .over((ob: any) =>
+            func('person.id')
+              .over((ob) =>
                 ob
                   .orderBy('person.last_name', 'asc')
                   .orderBy('person.first_name', 'asc')
               )
-              .as(func),
-            (qb: any) =>
-              qb.fn[func]('person.id')
-                .over((ob: any) =>
+              .as(funcName),
+            (eb) =>
+              getFuncFromExpressionBuilder(
+                eb,
+                funcName
+              )('person.id')
+                .over((ob) =>
                   ob
                     .orderBy('person.last_name', 'desc')
                     .orderBy('person.first_name', 'desc')
                 )
-                .as(`another_${func}`),
+                .as(`another_${funcName}`),
           ])
 
           testSql(query, dialect, {
             postgres: {
-              sql: `select ${func}("person"."id") over(order by "person"."last_name" asc, "person"."first_name" asc) as "${func}", ${func}("person"."id") over(order by "person"."last_name" desc, "person"."first_name" desc) as "another_${func}" from "person"`,
+              sql: `select ${funcName}("person"."id") over(order by "person"."last_name" asc, "person"."first_name" asc) as "${funcName}", ${funcName}("person"."id") over(order by "person"."last_name" desc, "person"."first_name" desc) as "another_${funcName}" from "person"`,
               parameters: [],
             },
             mysql: {
-              sql: `select ${func}(\`person\`.\`id\`) over(order by \`person\`.\`last_name\` asc, \`person\`.\`first_name\` asc) as \`${func}\`, ${func}(\`person\`.\`id\`) over(order by \`person\`.\`last_name\` desc, \`person\`.\`first_name\` desc) as \`another_${func}\` from \`person\``,
+              sql: `select ${funcName}(\`person\`.\`id\`) over(order by \`person\`.\`last_name\` asc, \`person\`.\`first_name\` asc) as \`${funcName}\`, ${funcName}(\`person\`.\`id\`) over(order by \`person\`.\`last_name\` desc, \`person\`.\`first_name\` desc) as \`another_${funcName}\` from \`person\``,
               parameters: [],
             },
             sqlite: {
-              sql: `select ${func}("person"."id") over(order by "person"."last_name" asc, "person"."first_name" asc) as "${func}", ${func}("person"."id") over(order by "person"."last_name" desc, "person"."first_name" desc) as "another_${func}" from "person"`,
+              sql: `select ${funcName}("person"."id") over(order by "person"."last_name" asc, "person"."first_name" asc) as "${funcName}", ${funcName}("person"."id") over(order by "person"."last_name" desc, "person"."first_name" desc) as "another_${funcName}" from "person"`,
               parameters: [],
             },
           })
@@ -166,38 +194,41 @@ for (const dialect of BUILT_IN_DIALECTS) {
           await query.execute()
         })
 
-        it(`should execute a query with ${func}(...) over(partition by ... order by ...) in select clause`, async () => {
+        it(`should execute a query with ${funcName}(...) over(partition by ... order by ...) in select clause`, async () => {
           const query = ctx.db.selectFrom('person').select([
-            (ctx.db.fn[func] as Function)('person.id')
-              .over((ob: any) =>
+            func('person.id')
+              .over((ob) =>
                 ob
                   .partitionBy(['person.gender'])
                   .orderBy('person.last_name', 'asc')
                   .orderBy('person.first_name', 'asc')
               )
-              .as(func),
-            (qb: any) =>
-              qb.fn[func]('person.id')
-                .over((ob: any) =>
+              .as(funcName),
+            (eb) =>
+              getFuncFromExpressionBuilder(
+                eb,
+                funcName
+              )('person.id')
+                .over((ob) =>
                   ob
-                    .partitionBy(['person.gender'])
+                    .partitionBy('person.gender')
                     .orderBy('person.last_name', 'desc')
                     .orderBy('person.first_name', 'desc')
                 )
-                .as(`another_${func}`),
+                .as(`another_${funcName}`),
           ])
 
           testSql(query, dialect, {
             postgres: {
-              sql: `select ${func}("person"."id") over(partition by "person"."gender" order by "person"."last_name" asc, "person"."first_name" asc) as "${func}", ${func}("person"."id") over(partition by "person"."gender" order by "person"."last_name" desc, "person"."first_name" desc) as "another_${func}" from "person"`,
+              sql: `select ${funcName}("person"."id") over(partition by "person"."gender" order by "person"."last_name" asc, "person"."first_name" asc) as "${funcName}", ${funcName}("person"."id") over(partition by "person"."gender" order by "person"."last_name" desc, "person"."first_name" desc) as "another_${funcName}" from "person"`,
               parameters: [],
             },
             mysql: {
-              sql: `select ${func}(\`person\`.\`id\`) over(partition by \`person\`.\`gender\` order by \`person\`.\`last_name\` asc, \`person\`.\`first_name\` asc) as \`${func}\`, ${func}(\`person\`.\`id\`) over(partition by \`person\`.\`gender\` order by \`person\`.\`last_name\` desc, \`person\`.\`first_name\` desc) as \`another_${func}\` from \`person\``,
+              sql: `select ${funcName}(\`person\`.\`id\`) over(partition by \`person\`.\`gender\` order by \`person\`.\`last_name\` asc, \`person\`.\`first_name\` asc) as \`${funcName}\`, ${funcName}(\`person\`.\`id\`) over(partition by \`person\`.\`gender\` order by \`person\`.\`last_name\` desc, \`person\`.\`first_name\` desc) as \`another_${funcName}\` from \`person\``,
               parameters: [],
             },
             sqlite: {
-              sql: `select ${func}("person"."id") over(partition by "person"."gender" order by "person"."last_name" asc, "person"."first_name" asc) as "${func}", ${func}("person"."id") over(partition by "person"."gender" order by "person"."last_name" desc, "person"."first_name" desc) as "another_${func}" from "person"`,
+              sql: `select ${funcName}("person"."id") over(partition by "person"."gender" order by "person"."last_name" asc, "person"."first_name" asc) as "${funcName}", ${funcName}("person"."id") over(partition by "person"."gender" order by "person"."last_name" desc, "person"."first_name" desc) as "another_${funcName}" from "person"`,
               parameters: [],
             },
           })
@@ -205,47 +236,47 @@ for (const dialect of BUILT_IN_DIALECTS) {
           await query.execute()
         })
 
-        it(`should execute a query with ${func}(...) in having clause`, async () => {
+        it(`should execute a query with ${funcName}(...) in having clause`, async () => {
           const query = ctx.db
             .selectFrom('person')
             .selectAll()
             .groupBy(['person.gender'])
-            .having((ctx.db.fn[func] as Function)('person.id'), '>=', 3)
+            .having(func('person.id'), '>=', 3)
 
           testSql(query, dialect, {
             postgres: {
-              sql: `select * from "person" group by "person"."gender" having ${func}("person"."id") >= $1`,
+              sql: `select * from "person" group by "person"."gender" having ${funcName}("person"."id") >= $1`,
               parameters: [3],
             },
             mysql: {
-              sql: `select * from \`person\` group by \`person\`.\`gender\` having ${func}(\`person\`.\`id\`) >= ?`,
+              sql: `select * from \`person\` group by \`person\`.\`gender\` having ${funcName}(\`person\`.\`id\`) >= ?`,
               parameters: [3],
             },
             sqlite: {
-              sql: `select * from "person" group by "person"."gender" having ${func}("person"."id") >= ?`,
+              sql: `select * from "person" group by "person"."gender" having ${funcName}("person"."id") >= ?`,
               parameters: [3],
             },
           })
         })
 
-        it(`should execute a query with ${func}(...) in order by clause`, async () => {
+        it(`should execute a query with ${funcName}(...) in order by clause`, async () => {
           const query = ctx.db
             .selectFrom('person')
             .selectAll()
             .groupBy(['person.gender'])
-            .orderBy((ctx.db.fn[func] as Function)('person.id'), 'desc')
+            .orderBy(func('person.id'), 'desc')
 
           testSql(query, dialect, {
             postgres: {
-              sql: `select * from "person" group by "person"."gender" order by ${func}("person"."id") desc`,
+              sql: `select * from "person" group by "person"."gender" order by ${funcName}("person"."id") desc`,
               parameters: [],
             },
             mysql: {
-              sql: `select * from \`person\` group by \`person\`.\`gender\` order by ${func}(\`person\`.\`id\`) desc`,
+              sql: `select * from \`person\` group by \`person\`.\`gender\` order by ${funcName}(\`person\`.\`id\`) desc`,
               parameters: [],
             },
             sqlite: {
-              sql: `select * from "person" group by "person"."gender" order by ${func}("person"."id") desc`,
+              sql: `select * from "person" group by "person"."gender" order by ${funcName}("person"."id") desc`,
               parameters: [],
             },
           })
@@ -254,3 +285,22 @@ for (const dialect of BUILT_IN_DIALECTS) {
     }
   })
 }
+
+function getFuncFromTestContext<TB extends keyof Database>(
+  ctx: TestContext,
+  funcName: typeof funcNames[number]
+): AggregateFunction<TB> {
+  return ctx.db.fn[funcName] as any
+}
+
+function getFuncFromExpressionBuilder<TB extends keyof Database>(
+  eb: ExpressionBuilder<Database, TB>,
+  funcName: typeof funcNames[number]
+): AggregateFunction<TB> {
+  return eb.fn[funcName] as any
+}
+
+type AggregateFunction<
+  TB extends keyof Database,
+  C extends StringReference<Database, TB> = StringReference<Database, TB>
+> = (column: C) => AggregateFunctionBuilder<Database, TB>
