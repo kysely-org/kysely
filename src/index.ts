d5ec3da614a08993369f45c391d9fd8f5dca2094
Igal Klebanov <igalklebanov@gmail.com>
2022-09-25 00:46:05 +0300

    bad export is bad.

--- src/index.ts
+++ src/index.ts
@@ -177,7 +177,7 @@ export * from './util/column-type.js'
 export * from './util/compilable.js'
 export * from './util/explainable.js'
 export * from './util/log.js'
-export { QueryId } from './util/query-id'
+export { QueryId } from './util/query-id.js'
 export {
   AnyColumn,
   UnknownRow,
