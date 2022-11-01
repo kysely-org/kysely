f69b96e0b62b3032bd5c42a050d469019744291b
Igal Klebanov <igalklebanov@gmail.com>
2022-11-01 13:22:55 +0200

    fix infer typings tests following rename.

--- test/typings/test-d/infer-result.test-d.ts
+++ test/typings/test-d/infer-result.test-d.ts
@@ -2,7 +2,7 @@ import { expectType } from 'tsd'
 
 import {
   DeleteResult,
-  Infer,
+  InferResult,
   InsertResult,
   Kysely,
   Selectable,
@@ -10,20 +10,20 @@ import {
 } from '..'
 import { Database, Equals, Person, Pet } from '../shared'
 
-function testInferSelectQuery(db: Kysely<Database>) {
+function testInferResultSelectQuery(db: Kysely<Database>) {
   const query0 = db.selectFrom('person').selectAll()
   const compiledQuery0 = query0.compile()
 
   type Expected0 = Selectable<Person>[]
-  expectType<Equals<Expected0, Infer<typeof query0>>>(true)
-  expectType<Equals<Expected0, Infer<typeof compiledQuery0>>>(true)
+  expectType<Equals<Expected0, InferResult<typeof query0>>>(true)
+  expectType<Equals<Expected0, InferResult<typeof compiledQuery0>>>(true)
 
   const query1 = db.selectFrom('person').select(['id', 'first_name'])
   const compiledQuery1 = query1.compile()
 
   type Expected1 = { id: number; first_name: string }[]
-  expectType<Equals<Expected1, Infer<typeof query1>>>(true)
-  expectType<Equals<Expected1, Infer<typeof compiledQuery1>>>(true)
+  expectType<Equals<Expected1, InferResult<typeof query1>>>(true)
+  expectType<Equals<Expected1, InferResult<typeof compiledQuery1>>>(true)
 
   const query2 = db
     .selectFrom('person')
@@ -32,11 +32,11 @@ function testInferSelectQuery(db: Kysely<Database>) {
   const compiledQuery2 = query2.compile()
 
   type Expected2 = { first_name: string; name: string }[]
-  expectType<Equals<Expected2, Infer<typeof query2>>>(true)
-  expectType<Equals<Expected2, Infer<typeof compiledQuery2>>>(true)
+  expectType<Equals<Expected2, InferResult<typeof query2>>>(true)
+  expectType<Equals<Expected2, InferResult<typeof compiledQuery2>>>(true)
 }
 
-function testInferInsertQuery(db: Kysely<Database>) {
+function testInferResultInsertQuery(db: Kysely<Database>) {
   const query0 = db.insertInto('person').values({
     first_name: 'Foo',
     last_name: 'Barson',
@@ -46,25 +46,25 @@ function testInferInsertQuery(db: Kysely<Database>) {
   const compiledQuery0 = query0.compile()
 
   type Expected0 = InsertResult
-  expectType<Equals<Expected0, Infer<typeof query0>>>(true)
-  expectType<Equals<Expected0, Infer<typeof compiledQuery0>>>(true)
+  expectType<Equals<Expected0, InferResult<typeof query0>>>(true)
+  expectType<Equals<Expected0, InferResult<typeof compiledQuery0>>>(true)
 
   const query1 = query0.returningAll()
   const compiledQuery1 = query1.compile()
 
   type Expected1 = Selectable<Person>[]
-  expectType<Equals<Expected1, Infer<typeof query1>>>(true)
-  expectType<Equals<Expected1, Infer<typeof compiledQuery1>>>(true)
+  expectType<Equals<Expected1, InferResult<typeof query1>>>(true)
+  expectType<Equals<Expected1, InferResult<typeof compiledQuery1>>>(true)
 
   const query2 = query0.returning('modified_at')
   const compiledQuery2 = query2.compile()
 
   type Expected2 = { modified_at: Date }[]
-  expectType<Equals<Expected2, Infer<typeof query2>>>(true)
-  expectType<Equals<Expected2, Infer<typeof compiledQuery2>>>(true)
+  expectType<Equals<Expected2, InferResult<typeof query2>>>(true)
+  expectType<Equals<Expected2, InferResult<typeof compiledQuery2>>>(true)
 }
 
-function testInferUpdateQuery(db: Kysely<Database>) {
+function testInferResultUpdateQuery(db: Kysely<Database>) {
   const query0 = db
     .updateTable('person')
     .innerJoin('pet', 'pet.owner_id', 'person.id')
@@ -73,43 +73,43 @@ function testInferUpdateQuery(db: Kysely<Database>) {
   const compiledQuery0 = query0.compile()
 
   type Expected0 = UpdateResult
-  expectType<Equals<Expected0, Infer<typeof query0>>>(true)
-  expectType<Equals<Expected0, Infer<typeof compiledQuery0>>>(true)
+  expectType<Equals<Expected0, InferResult<typeof query0>>>(true)
+  expectType<Equals<Expected0, InferResult<typeof compiledQuery0>>>(true)
 
   const query1 = query0.returningAll()
   const compiledQuery1 = query1.compile()
 
   type Expected1 = Selectable<Person | Pet>[]
-  expectType<Equals<Expected1, Infer<typeof query1>>>(true)
-  expectType<Equals<Expected1, Infer<typeof compiledQuery1>>>(true)
+  expectType<Equals<Expected1, InferResult<typeof query1>>>(true)
+  expectType<Equals<Expected1, InferResult<typeof compiledQuery1>>>(true)
 
   const query2 = query0.returning('modified_at')
   const compiledQuery2 = query2.compile()
 
   type Expected2 = { modified_at: Date }[]
-  expectType<Equals<Expected2, Infer<typeof query2>>>(true)
-  expectType<Equals<Expected2, Infer<typeof compiledQuery2>>>(true)
+  expectType<Equals<Expected2, InferResult<typeof query2>>>(true)
+  expectType<Equals<Expected2, InferResult<typeof compiledQuery2>>>(true)
 }
 
-function testInferDeleteQuery(db: Kysely<Database>) {
+function testInferResultDeleteQuery(db: Kysely<Database>) {
   const query0 = db.deleteFrom('pet').where('id', '=', '1')
   const compiledQuery0 = query0.compile()
 
   type Expected0 = DeleteResult
-  expectType<Equals<Expected0, Infer<typeof query0>>>(true)
-  expectType<Equals<Expected0, Infer<typeof compiledQuery0>>>(true)
+  expectType<Equals<Expected0, InferResult<typeof query0>>>(true)
+  expectType<Equals<Expected0, InferResult<typeof compiledQuery0>>>(true)
 
   const query1 = query0.returningAll()
   const compiledQuery1 = query1.compile()
 
   type Expected1 = Selectable<Pet>[]
-  expectType<Equals<Expected1, Infer<typeof query1>>>(true)
-  expectType<Equals<Expected1, Infer<typeof compiledQuery1>>>(true)
+  expectType<Equals<Expected1, InferResult<typeof query1>>>(true)
+  expectType<Equals<Expected1, InferResult<typeof compiledQuery1>>>(true)
 
   const query2 = query0.returning('id')
   const compiledQuery2 = query2.compile()
 
   type Expected2 = { id: string }[]
-  expectType<Equals<Expected2, Infer<typeof query2>>>(true)
-  expectType<Equals<Expected2, Infer<typeof compiledQuery2>>>(true)
+  expectType<Equals<Expected2, InferResult<typeof query2>>>(true)
+  expectType<Equals<Expected2, InferResult<typeof compiledQuery2>>>(true)
 }
