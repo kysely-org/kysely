8e8b02bc616d76cb15d1596f5c0498cf52353e6a
Igal Klebanov <igalklebanov@gmail.com>
2022-11-04 11:45:25 +0200

    simplify ReadonlyConnectionBuilder.

--- src/kysely.ts
+++ src/kysely.ts
@@ -561,7 +561,7 @@ export type ReadonlyKysely<DB> = Pick<
     >
   }
 
-type ReadonlyConnectionBuilder<DB> = Omit<ConnectionBuilder<DB>, 'execute'> & {
+type ReadonlyConnectionBuilder<DB> = {
   execute<T>(db: ReadonlyKysely<DB>): Promise<T>
 }
 
