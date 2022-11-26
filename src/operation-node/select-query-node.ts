9c9acc6a4dbaf90b178397d3ceafdc22d3cb2b46
Naor Peled <thehecticbyte@gmail.com>
2022-11-26 10:29:26 +0200

    feat(select/distinctOn): add RawBuilder support (#239)
    closes #230

--- src/operation-node/select-query-node.ts
+++ src/operation-node/select-query-node.ts
@@ -15,12 +15,13 @@ import { WithNode } from './with-node.js'
 import { SelectModifierNode } from './select-modifier-node.js'
 import { ExplainNode } from './explain-node.js'
 import { SetOperationNode } from './set-operation-node.js'
+import { SimpleReferenceExpressionNode } from './simple-reference-expression-node.js'
 
 export interface SelectQueryNode extends OperationNode {
   readonly kind: 'SelectQueryNode'
   readonly from: FromNode
   readonly selections?: ReadonlyArray<SelectionNode>
-  readonly distinctOnSelections?: ReadonlyArray<SelectionNode>
+  readonly distinctOnSelections?: ReadonlyArray<SimpleReferenceExpressionNode>
   readonly joins?: ReadonlyArray<JoinNode>
   readonly groupBy?: GroupByNode
   readonly orderBy?: OrderByNode
@@ -68,7 +69,7 @@ export const SelectQueryNode = freeze({
 
   cloneWithDistinctOnSelections(
     select: SelectQueryNode,
-    selections: ReadonlyArray<SelectionNode>
+    selections: ReadonlyArray<SimpleReferenceExpressionNode>
   ): SelectQueryNode {
     return freeze({
       ...select,
