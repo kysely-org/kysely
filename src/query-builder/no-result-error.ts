3de7cdae6f1d17edcc94ad6f580394f06d2caa5d
Igal Klebanov <igalklebanov@gmail.com>
2022-11-26 20:46:24 +0200

    expand `NoResultError`'s node to `RawNode` as well.

--- src/query-builder/no-result-error.ts
+++ src/query-builder/no-result-error.ts
@@ -1,14 +1,15 @@
 import { QueryNode } from '../operation-node/query-node.js'
+import { RawNode } from '../operation-node/raw-node.js'
 
-export type NoResultErrorConstructor = new (node: QueryNode) => Error
+export type NoResultErrorConstructor = new (node: QueryNode | RawNode) => Error
 
 export class NoResultError extends Error {
   /**
    * The operation node tree of the query that was executed.
    */
-  readonly node: QueryNode
+  readonly node: QueryNode | RawNode
 
-  constructor(node: QueryNode) {
+  constructor(node: QueryNode | RawNode) {
     super('no result')
     this.node = node
   }
