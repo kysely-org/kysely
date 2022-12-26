bb59716a616f01c71a75ee1e6ac25468260e956e
wirekang <wirekang@gmail.com>
2022-12-26 17:37:56 +0900

    test(clear): add more test

--- test/typings/test-d/clear.test-d.ts
+++ test/typings/test-d/clear.test-d.ts
@@ -3,12 +3,21 @@ import { Database } from '../shared'
 import { expectType } from 'tsd'
 
 async function testClearSelect(db: Kysely<Database>) {
-  const row = await db
+  const r1 = await db
     .selectFrom('person')
     .select(['first_name', 'gender'])
     .clearSelect()
     .select('id')
     .executeTakeFirstOrThrow()
 
-  expectType<{ id: number }>(row)
+  expectType<{ id: number }>(r1)
+
+  const r2 = await db
+      .selectFrom('person')
+      .selectAll()
+      .clearSelect()
+      .select('age')
+      .executeTakeFirstOrThrow()
+
+  expectType<{ age: number }>(r2)
 }
