a3f35a7a9d3ad9504ec225a2f32d5975118008c3
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-01-22 18:57:58 +0200

    docs(plugin): fixed and refactor example for KyselyPlugin.

--- src/plugin/kysely-plugin.ts
+++ src/plugin/kysely-plugin.ts
@@ -13,21 +13,25 @@ export interface KyselyPlugin {
    * If you need to pass some query-related data between this method and `transformResult` you
    * can use a `WeakMap` with {@link PluginTransformQueryArgs.queryId | args.queryId} as the key:
    *
-   * ```ts
-   * const plugin = {
-   *   data: new WeakMap<QueryId, SomeData>(),
-   *
-   *   transformQuery(args: PluginTransformQueryArgs): RootOperationNode {
-   *     this.data.set(args.queryId, something)
-   *     return args.node
-   *   },
-   *
-   *   transformResult(args: PluginTransformResultArgs): QueryResult<UnknownRow> {
-   *     const data = this.data.get(args.queryId)
-   *     return args.result
-   *   }
-   * }
-   * ```
+```ts
+import { KyselyPlugin, PluginTransformQueryArgs, PluginTransformResultArgs } from 'kysely';
+
+class MyPlugin implements KyselyPlugin {
+  private data = new WeakMap<QueryId, SomeData>();
+
+  transformQuery(args: PluginTransformQueryArgs) {
+    this.data.set(args.queryId, something);
+    return args.node;
+  }
+
+  async transformResult(args: PluginTransformResultArgs) {
+    const data = this.data.get(args.queryId);
+    return args.result;
+  }
+}
+
+const myPlugin = new MyPlugin();
+```
    *
    * You should use a `WeakMap` instead of a `Map` or some other strong references because `transformQuery`
    * is not always matched by a call to `transformResult` which would leave orphaned items in the map
