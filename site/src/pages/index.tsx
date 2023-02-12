9d05b72b0b75a6107318daefff170d855084de17
Fernando Hurtado <fernandohur@gmail.com>
2023-02-12 19:48:49 +0100

    fix title

--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -340,7 +340,7 @@ function SectionPlayground() {
             background: gray.gray12,
             borderRadius: 8,
           }}
-          title="kysely-demo"
+          title="Kysely Demo"
           allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
           sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
         />
