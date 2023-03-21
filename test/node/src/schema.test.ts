15abc7c538be1781e598157c14d23d543159861b
Igal Klebanov <igalklebanov@gmail.com>
2023-03-21 23:57:36 +0200

    add sorted index test cases.

--- test/node/src/schema.test.ts
+++ test/node/src/schema.test.ts
@@ -1071,6 +1071,54 @@ for (const dialect of DIALECTS) {
 
         await builder.execute()
       })
+
+      it('should create a sorted index, single column', async () => {
+        const builder = ctx.db.schema
+          .createIndex('test_descending_first_name_index')
+          .on('test')
+          .column('first_name desc')
+
+        testSql(builder, dialect, {
+          postgres: {
+            sql: 'create index "test_descending_first_name_index" on "test" ("first_name" desc)',
+            parameters: [],
+          },
+          mysql: {
+            sql: 'create index `test_descending_first_name_index` on `test` (`first_name` desc)',
+            parameters: [],
+          },
+          sqlite: {
+            sql: 'create index "test_descending_first_name_index" on "test" ("first_name" desc)',
+            parameters: [],
+          },
+        })
+
+        await builder.execute()
+      })
+
+      it('should create a sorted index, multi-column', async () => {
+        const builder = ctx.db.schema
+          .createIndex('test_first_name_descending_last_name_index')
+          .on('test')
+          .columns(['first_name', 'last_name desc'])
+
+        testSql(builder, dialect, {
+          postgres: {
+            sql: 'create index "test_first_name_descending_last_name_index" on "test" ("first_name", "last_name" desc)',
+            parameters: [],
+          },
+          mysql: {
+            sql: 'create index `test_first_name_descending_last_name_index` on `test` (`first_name`, `last_name` desc)',
+            parameters: [],
+          },
+          sqlite: {
+            sql: 'create index "test_first_name_descending_last_name_index" on "test" ("first_name", "last_name" desc)',
+            parameters: [],
+          },
+        })
+
+        await builder.execute()
+      })
     })
 
     describe('drop index', () => {
