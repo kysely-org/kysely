0cdd74b284affeb3bcbb688a07fbc76cef415efc
anirudh1713 <anirudhatodaria11@gmail.com>
2023-04-08 00:26:00 +0530

    feat: add support for `char` datatype without passing size

--- test/node/src/schema.test.ts
+++ test/node/src/schema.test.ts
@@ -72,6 +72,7 @@ for (const dialect of DIALECTS) {
             )
             .addColumn('v', 'timestamptz(6)')
             .addColumn('w', 'char(4)')
+            .addColumn('x', 'char')
 
           testSql(builder, dialect, {
             postgres: {
@@ -99,7 +100,8 @@ for (const dialect of DIALECTS) {
                 '"t" time(6),',
                 '"u" timestamp(6) default current_timestamp not null,',
                 '"v" timestamptz(6),',
-                '"w" char(4))',
+                '"w" char(4),',
+                '"x" char)'
               ],
               parameters: [],
             },
@@ -173,6 +175,7 @@ for (const dialect of DIALECTS) {
               col.notNull().defaultTo(sql`current_timestamp(6)`)
             )
             .addColumn('t', 'char(4)')
+            .addColumn('u', 'char')
 
           testSql(builder, dialect, {
             mysql: {
@@ -197,7 +200,8 @@ for (const dialect of DIALECTS) {
                 '`q` time(6),',
                 '`r` datetime(6),',
                 '`s` timestamp(6) default current_timestamp(6) not null,',
-                '`t` char(4))',
+                '`t` char(4),',
+                '`u` char)'
               ],
               parameters: [],
             },
