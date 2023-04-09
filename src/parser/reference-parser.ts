22ec6d21c739c46e77ad62ab4e131b34aef389f5
Igal Klebanov <igalklebanov@gmail.com>
2023-04-09 12:22:54 +0300

    add column sort order @ `CreateIndexBuilder`. (#375)
    * add column sort order to `CreateIndexBuilder`.
    
    * add sorted index test cases.
    
    * remove `any`.
    
    Co-authored-by: Sami Koskimäki <846508+koskimas@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Sami Koskimäki <846508+koskimas@users.noreply.github.com>
    Co-authored-by: Sami Koskimäki <koskomi@gmail.com>

--- src/parser/reference-parser.ts
+++ src/parser/reference-parser.ts
@@ -21,6 +21,11 @@ import { IdentifierNode } from '../operation-node/identifier-node.js'
 import { OperationNode } from '../operation-node/operation-node.js'
 import { Expression } from '../expression/expression.js'
 import { SimpleReferenceExpressionNode } from '../operation-node/simple-reference-expression-node.js'
+import {
+  OrderByDirection,
+  isOrderByDirection,
+  parseOrderBy,
+} from './order-by-parser.js'
 
 export type StringReference<DB, TB extends keyof DB> =
   | AnyColumn<DB, TB>
@@ -75,6 +80,20 @@ export type ExtractTypeFromStringReference<
   : RE extends AnyColumn<DB, TB>
   ? ExtractColumnType<DB, TB, RE>
   : DV
+
+export type OrderedColumnName<C extends string> =
+  C extends `${string} ${infer O}`
+    ? O extends OrderByDirection
+      ? C
+      : never
+    : C
+
+export type ExtractColumnNameFromOrderedColumnName<C extends string> =
+  C extends `${infer CL} ${infer O}`
+    ? O extends OrderByDirection
+      ? CL
+      : never
+    : C
 
 export function parseSimpleReferenceExpression(
   exp: SimpleReferenceExpression<any, any>
@@ -145,6 +164,24 @@ export function parseAliasedStringReference(
 
 export function parseColumnName(column: AnyColumn<any, any>): ColumnNode {
   return ColumnNode.create(column)
+}
+
+export function parseOrderedColumnName(column: string): OperationNode {
+  const ORDER_SEPARATOR = ' '
+
+  if (column.includes(ORDER_SEPARATOR)) {
+    const [columnName, order] = column.split(ORDER_SEPARATOR).map(trim)
+
+    if (!isOrderByDirection(order)) {
+      throw new Error(
+        `invalid order direction "${order}" next to "${columnName}"`
+      )
+    }
+
+    return parseOrderBy(columnName, order)
+  } else {
+    return parseColumnName(column)
+  }
 }
 
 function parseStringReferenceWithTableAndSchema(
