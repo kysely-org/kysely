5036a74f741316d6cc0d2f0a336e698313df66e1
Igal Klebanov <igalklebanov@gmail.com>
2023-04-22 20:31:35 +0300

    add JSON operators.

--- src/operation-node/operator-node.ts
+++ src/operation-node/operator-node.ts
@@ -50,9 +50,17 @@ export const ARITHMETIC_OPERATORS = [
   '>>',
 ] as const
 
+export const JSON_OPERATORS = [
+  '->',
+  '->>',
+  // '#>',
+  // '#>>'
+] as const
+
 export const BINARY_OPERATORS = [
   ...COMPARISON_OPERATORS,
   ...ARITHMETIC_OPERATORS,
+  ...JSON_OPERATORS,
   '&&',
   '||',
 ] as const
@@ -63,6 +71,7 @@ export const OPERATORS = [...BINARY_OPERATORS, ...UNARY_OPERATORS] as const
 
 export type ComparisonOperator = typeof COMPARISON_OPERATORS[number]
 export type ArithmeticOperator = typeof ARITHMETIC_OPERATORS[number]
+export type JSONOperator = typeof JSON_OPERATORS[number]
 export type BinaryOperator = typeof BINARY_OPERATORS[number]
 export type UnaryOperator = typeof UNARY_OPERATORS[number]
 export type UnaryFilterOperator = typeof UNARY_FILTER_OPERATORS[number]
@@ -103,4 +112,8 @@ export function isComparisonOperator(op: unknown): op is ComparisonOperator {
 
 export function isArithmeticOperator(op: unknown): op is ArithmeticOperator {
   return isString(op) && ARITHMETIC_OPERATORS.includes(op as ArithmeticOperator)
+}
+
+export function isJSONOperator(op: unknown): op is JSONOperator {
+  return isString(op) && JSON_OPERATORS.includes(op as JSONOperator)
 }
