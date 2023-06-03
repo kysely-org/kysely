3a27342ab00fddf8d19d038d72b76cd2e8618976
Igal Klebanov <igalklebanov@gmail.com>
2023-06-03 05:16:13 +0300

    wrong import in migrations.

--- site/docs/getting-started/Migrations.tsx
+++ site/docs/getting-started/Migrations.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from '@docusaurus/Link'
 import CodeBlock from '@theme/CodeBlock'
-import type { Dialect, PropsWithDialect } from './types'
+import type { Dialect, PropsWithDialect } from './shared'
 
 const dialectSpecificCodeSnippets: Record<
   Dialect,
