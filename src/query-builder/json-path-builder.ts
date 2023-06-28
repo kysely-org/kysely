aa97a6da3cac30bcbd49acfdda2eeb54667276ab
igalklebanov <igalklebanov@gmail.com>
2023-06-29 02:41:20 +0300

    add json path builder js docs.

--- src/query-builder/json-path-builder.ts
+++ src/query-builder/json-path-builder.ts
@@ -19,6 +19,70 @@ export class JSONPathBuilder<S, O = S> {
     this.#node = node
   }
 
+  /**
+   * Access an element of a JSON array in a specific location.
+   *
+   * Since there's no guarantee an element exists in the given array location, the
+   * resulting type is always nullable. If you're sure the element exists, you
+   * should use {@link SelectQueryBuilder.$assertType} to narrow the type safely.
+   *
+   * See also {@link key} to access fields in JSON objects.
+   *
+   * ### Examples
+   *
+   * ```ts
+   * db.selectFrom('person').select(eb =>
+   *   eb.ref('nicknames', '->>').at(0).as('primary_nickname')
+   * )
+   *
+   * The generated SQL (PostgreSQL):
+   *
+   * ```sql
+   * select "nicknames"->>0 as "primary_nickname" from "person"
+   *```
+   *
+   * Combined with {@link key}:
+   *
+   * ```ts
+   * db.selectFrom('person').select(eb =>
+   *   eb.ref('experience', '->>').at(0).key('role').as('first_role')
+   * )
+   * ```
+   *
+   * The generated SQL (PostgreSQL):
+   *
+   * ```sql
+   * select "experience"->>0->'role' as "first_role" from "person"
+   * ```
+   *
+   * You can use `'last'` to access the last element of the array in MySQL:
+   *
+   * ```ts
+   * db.selectFrom('person').select(eb =>
+   *   eb.ref('nicknames', '->>$').at('last').as('last_nickname')
+   * )
+   * ```
+   *
+   * The generated SQL (MySQL):
+   *
+   * ```sql
+   * select `nicknames`->'$[last]' as `last_nickname` from `person`
+   * ```
+   *
+   * Or `'#-1'` in SQLite:
+   *
+   * ```ts
+   * db.selectFrom('person').select(eb =>
+   *   eb.ref('nicknames', '->>$').at('#-1').as('last_nickname')
+   * )
+   * ```
+   *
+   * The generated SQL (SQLite):
+   *
+   * ```sql
+   * select "nicknames"->'$[#-1]' as `last_nickname` from `person`
+   * ```
+   */
   at<
     I extends any[] extends O ? number | 'last' | `#-${number}` : never,
     O2 = null | NonNullable<NonNullable<O>[keyof NonNullable<O> & number]>
@@ -28,6 +92,55 @@ export class JSONPathBuilder<S, O = S> {
     return this.#createBuilderWithPathLeg('ArrayLocation', index)
   }
 
+  /**
+   * Access a field of a JSON object.
+   *
+   * If a field is optional, the resulting type will be nullable.
+   *
+   * See also {@link at} to access elements of JSON arrays.
+   *
+   * ### Examples
+   *
+   * ```ts
+   * db.selectFrom('person').select(eb =>
+   *   eb.ref('address', '->>').key('city').as('city')
+   * )
+   * ```
+   *
+   * The generated SQL (PostgreSQL):
+   *
+   * ```sql
+   * select "address"->'city' as "city" from "person"
+   * ```
+   *
+   * Going deeper:
+   *
+   * ```ts
+   * db.selectFrom('person').select(eb =>
+   *   eb.ref('profile', '->>$').key('website').key('url').as('website_url')
+   * )
+   * ```
+   *
+   * The generated SQL (MySQL):
+   *
+   * ```sql
+   * select `profile`->'$.website.url' as `website_url` from `person`
+   * ```
+   *
+   * Combined with {@link at}:
+   *
+   * ```ts
+   * db.selectFrom('person').select(eb =>
+   *   eb.ref('profile', '->>').key('addresses').at(0).key('city').as('city')
+   * )
+   * ```
+   *
+   * The generated SQL (PostgreSQL):
+   *
+   * ```sql
+   * select "profile"->'addresses'->0->'city' as "city" from "person"
+   * ```
+   */
   key<
     K extends any[] extends O
       ? never
