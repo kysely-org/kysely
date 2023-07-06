ab7882fda82c8ebfdd454f2f6fd13e14f8661114
Igal Klebanov <igalklebanov@gmail.com>
2023-07-06 08:46:14 +0300

    test deno with real drivers. (#525)
    * bump driver dependencies.
    
    * test real drivers in deno tests.
    
    * upgrade deno in workflows.
    
    * readme pass.
    
    ---------
    
    Co-authored-by: Sami Koskimäki <koskomi@gmail.com>

--- test/deno/local.test.ts
+++ test/deno/local.test.ts
@@ -1,11 +1,12 @@
 import {
-  DummyDriver,
   Generated,
   Kysely,
-  PostgresAdapter,
-  PostgresIntrospector,
-  PostgresQueryCompiler,
+  PostgresDialect,
+  MysqlDialect,
+  sql,
 } from '../../dist/esm/index.js'
+import { createPool } from 'mysql2'
+import Pool from 'pg-pool'
 
 interface Person {
   id: Generated<number>
@@ -17,29 +18,43 @@ interface Database {
   person: Person
 }
 
-const db = new Kysely<Database>({
-  dialect: {
-    createAdapter() {
-      return new PostgresAdapter()
-    },
-    createDriver() {
-      return new DummyDriver()
-    },
-    createIntrospector(db: Kysely<unknown>) {
-      return new PostgresIntrospector(db)
-    },
-    createQueryCompiler() {
-      return new PostgresQueryCompiler()
-    },
-  },
-})
+const dbs = [
+  new Kysely<Database>({
+    dialect: new PostgresDialect({
+      pool: new Pool({
+        database: 'kysely_test',
+        host: 'localhost',
+        user: 'kysely',
+        port: 5434,
+      }),
+    }),
+  }),
+  new Kysely<Database>({
+    dialect: new MysqlDialect({
+      pool: createPool({
+        database: 'kysely_test',
+        host: 'localhost',
+        user: 'kysely',
+        password: 'kysely',
+        port: 3308,
+      }),
+    }),
+  }),
+]
 
-const query = db.selectFrom('person').select('id')
-const sql = query.compile()
-
-if (sql.sql !== 'select "id" from "person"') {
-  console.error('deno test failed')
+if (
+  dbs
+    .map((db) => db.selectFrom('person').select('id').compile().sql)
+    .some((sql) => sql.match(/^select ["`]id["`] from ["`]person["`]$/) == null)
+) {
+  console.error('CDN deno test failed')
   Deno.exit(1)
 }
 
+const query = sql`select 1`
+
+await Promise.all(dbs.map((db) => query.execute(db)))
+
 console.error('local deno test passed')
+
+await Promise.all(dbs.map((db) => db.destroy()))
