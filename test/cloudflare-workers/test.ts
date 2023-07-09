72184b379a26391215d63f6fa0f59b0ad9bfb2b0
igalklebanov <igalklebanov@gmail.com>
2023-07-10 00:35:12 +0300

    please

--- test/cloudflare-workers/test.ts
+++ test/cloudflare-workers/test.ts
@@ -1,40 +1,30 @@
-import { Pool } from 'pg'
-import { Generated, Kysely, PostgresDialect, sql } from '../../dist/cjs'
-
-interface Person {
-  id: Generated<number>
-  first_name: string
-  last_name: string | null
-}
-
-interface Database {
-  person: Person
-}
-
-const db = new Kysely<Database>({
-  dialect: new PostgresDialect({
-    pool: new Pool({
-      database: 'kysely_test',
-      host: 'localhost',
-      user: 'kysely',
-      port: 5434,
-    }),
-  }),
-})
-
-if (
-  db
-    .selectFrom('person')
-    .select('id')
-    .compile()
-    .sql.match(/^select "id" from "person"$/) === null
-) {
-  console.error('cloudflare workers test failed')
-  process.exit(1)
-}
-
+import { unstable_dev } from 'wrangler'
 ;(async () => {
-  await sql`select 1`.execute(db)
+  const worker = await unstable_dev('./api.ts', {
+    compatibilityDate: '2023-06-28',
+    experimental: { disableExperimentalWarning: true },
+    local: true,
+    // logLevel: 'debug',
+    nodeCompat: true,
+    vars: {
+      NAME: 'cloudflare',
+    },
+  })
 
-  process.exit(0)
+  let exitCode = 0
+
+  try {
+    const response = await worker.fetch('/')
+
+    if (!response.ok) {
+      throw new Error(`Unexpected response: ${response.status}`)
+    }
+
+    console.log('test successful!')
+  } catch (error) {
+    exitCode = 1
+  } finally {
+    await worker.stop()
+    process.exit(exitCode)
+  }
 })()
