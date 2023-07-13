e08d1e596afdf0170d2e960855097499c13df1c0
Sami Koskimäki <koskomi@gmail.com>
2023-07-13 10:22:51 +0300

    add object support for 'and' and 'or' functions. Closes #344

--- src/parser/binary-operation-parser.ts
+++ src/parser/binary-operation-parser.ts
@@ -1,18 +1,27 @@
 import { BinaryOperationNode } from '../operation-node/binary-operation-node.js'
-import { isBoolean, isNull, isString } from '../util/object-utils.js'
-import { isOperationNodeSource } from '../operation-node/operation-node-source.js'
+import {
+  isBoolean,
+  isNull,
+  isString,
+  isUndefined,
+} from '../util/object-utils.js'
+import {
+  OperationNodeSource,
+  isOperationNodeSource,
+} from '../operation-node/operation-node-source.js'
 import {
   OperatorNode,
   ComparisonOperator,
-  ArithmeticOperator,
   BinaryOperator,
   Operator,
   OPERATORS,
 } from '../operation-node/operator-node.js'
 import {
   ExtractTypeFromReferenceExpression,
+  ExtractTypeFromStringReference,
   parseReferenceExpression,
   ReferenceExpression,
+  StringReference,
 } from './reference-parser.js'
 import {
   parseValueExpression,
@@ -23,6 +32,10 @@ import {
 import { ValueNode } from '../operation-node/value-node.js'
 import { OperationNode } from '../operation-node/operation-node.js'
 import { Expression } from '../expression/expression.js'
+import { SelectType } from '../util/column-type.js'
+import { AndNode } from '../operation-node/and-node.js'
+import { ParensNode } from '../operation-node/parens-node.js'
+import { OrNode } from '../operation-node/or-node.js'
 
 export type OperandValueExpression<
   DB,
@@ -47,9 +60,13 @@ export type ComparisonOperatorExpression =
   | ComparisonOperator
   | Expression<unknown>
 
-export type ArithmeticOperatorExpression =
-  | ArithmeticOperator
-  | Expression<unknown>
+export type FilterObject<DB, TB extends keyof DB> = {
+  [R in StringReference<DB, TB>]?: ValueExpressionOrList<
+    DB,
+    TB,
+    SelectType<ExtractTypeFromStringReference<DB, TB, R>>
+  >
+}
 
 export function parseValueBinaryOperationOrExpression(
   args: any[]
@@ -68,7 +85,7 @@ export function parseValueBinaryOperation(
   operator: BinaryOperatorExpression,
   right: OperandValueExpressionOrList<any, any, any>
 ): BinaryOperationNode {
-  if (isIsOperator(operator) && isNullOrBoolean(right)) {
+  if (isIsOperator(operator) && needsIsOperator(right)) {
     return BinaryOperationNode.create(
       parseReferenceExpression(left),
       parseOperator(operator),
@@ -82,6 +99,7 @@ export function parseValueBinaryOperation(
     parseValueExpressionOrList(right)
   )
 }
+
 export function parseReferentialBinaryOperation(
   left: ReferenceExpression<any, any>,
   operator: BinaryOperatorExpression,
@@ -94,13 +112,50 @@ export function parseReferentialBinaryOperation(
   )
 }
 
+export function parseFilterObject(
+  obj: Readonly<FilterObject<any, any>>,
+  combinator: 'and' | 'or'
+): OperationNode {
+  return parseFilterList(
+    Object.entries(obj)
+      .filter(([, v]) => !isUndefined(v))
+      .map(([k, v]) =>
+        parseValueBinaryOperation(k, needsIsOperator(v) ? 'is' : '=', v)
+      ),
+    combinator
+  )
+}
+
+export function parseFilterList(
+  list: ReadonlyArray<OperationNodeSource | OperationNode>,
+  combinator: 'and' | 'or'
+): OperationNode {
+  const combine = combinator === 'and' ? AndNode.create : OrNode.create
+
+  if (list.length === 0) {
+    return ValueNode.createImmediate(combinator === 'and')
+  }
+
+  let node = toOperationNode(list[0])
+
+  for (let i = 1; i < list.length; ++i) {
+    node = combine(node, toOperationNode(list[i]))
+  }
+
+  if (list.length > 1) {
+    return ParensNode.create(node)
+  }
+
+  return node
+}
+
 function isIsOperator(
   operator: BinaryOperatorExpression
 ): operator is 'is' | 'is not' {
   return operator === 'is' || operator === 'is not'
 }
 
-function isNullOrBoolean(value: unknown): value is null | boolean {
+function needsIsOperator(value: unknown): value is null | boolean {
   return isNull(value) || isBoolean(value)
 }
 
@@ -114,4 +169,12 @@ function parseOperator(operator: OperatorExpression): OperationNode {
   }
 
   throw new Error(`invalid operator ${JSON.stringify(operator)}`)
+}
+
+function toOperationNode(
+  nodeOrSource: OperationNode | OperationNodeSource
+): OperationNode {
+  return isOperationNodeSource(nodeOrSource)
+    ? nodeOrSource.toOperationNode()
+    : nodeOrSource
 }
