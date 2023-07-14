795f96b4ad356387145dd1a7fa71aa67a58ac6f8
Sami Koskimäki <koskomi@gmail.com>
2023-07-14 08:19:59 +0300

    remove object filter site example for now

--- site/docs/examples/WHERE/0030-object-filter.js
+++
@@ -1,8 +0,0 @@
-export const objectFilter = `const persons = await db
-  .selectFrom('person')
-  .selectAll()
-  .where((eb) => eb.and({
-    first_name: 'Jennifer',
-    last_name: eb.ref('first_name')
-  }))
-  .execute()`
