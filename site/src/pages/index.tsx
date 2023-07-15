45b1f040292a6e482fe4bea541314bd37f8352f1
Igal Klebanov <igalklebanov@gmail.com>
2023-07-15 11:47:27 +0300

    add Orta and Tim Grissier quotes. (#591)

--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
 import clsx from 'clsx'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import Layout from '@theme/Layout'
@@ -128,6 +128,12 @@ export default function Home(): JSX.Element {
   )
 }
 
+const domainNameToIcon: Record<string, ReactElement> = {
+  twitter: <TwitterIcon />,
+  discord: <DiscordIcon />,
+  github: <GithubIcon />,
+}
+
 function Quote({
   avatar,
   authorName,
@@ -141,6 +147,8 @@ function Quote({
   authorTitle: string
   text: string
 }) {
+  const [, domainName] = link.toLowerCase().match(/https?:\/\/(?:www\.)?(.+)\./)
+
   return (
     <a
       href={link}
@@ -158,7 +166,7 @@ function Quote({
         textDecoration: 'none',
       }}
     >
-      {link.includes('twitter') ? <TwitterIcon /> : <DiscordIcon />}
+      {domainNameToIcon[domainName] || null}
       <div style={{ display: 'flex', gap: 8 }}>
         <img
           className="avatar__photo"
@@ -213,6 +221,18 @@ function SectionQuotes() {
           />
 
           <Quote
+            authorName={'Orta Therox'}
+            authorTitle={'Ex-TypeScript team'}
+            avatar={'/img/avatars/orta.jpeg'}
+            link={
+              'https://discord.com/channels/890118421587578920/890118421587578925/1037692748825903114'
+            }
+            text={
+              "👋 been using Kysely for a few days in Deno and Node, just wanted to say it's been going really well and it feels like a nice abstraction - kudos!"
+            }
+          />
+
+          <Quote
             authorName={'Ben Holmes'}
             authorTitle={'Astro SWE'}
             avatar={'/img/avatars/benholmes.jpeg'}
@@ -236,6 +256,18 @@ function SectionQuotes() {
             avatar={'/img/avatars/nexxel.jpeg'}
             link={'https://twitter.com/nexxeln/status/1676975946606452737'}
             text={'kysely is great btw'}
+          />
+
+          <Quote
+            authorName={'Tim Griesser'}
+            authorTitle={'Knex.js creator, Cypress SWE'}
+            avatar={'/img/avatars/tgriesser.jpeg'}
+            link={
+              'https://github.com/kysely-org/kysely/issues/352#issue-1612115150'
+            }
+            text={
+              "👋 First just wanted to say this project looks awesome! Pretty close to what I wish Knex was if I started it now, with the affordances of modern TypeScript instead of 10 years ago when we were stuck in ES5 and Promises weren't even really an official language construct yet."
+            }
           />
 
           <Quote
@@ -407,7 +439,7 @@ function TwitterIcon() {
 function DiscordIcon() {
   return (
     <svg
-      aria-hideen="true"
+      aria-hidden="true"
       viewBox="0 -28.5 256 256"
       version="1.1"
       xmlns="http://www.w3.org/2000/svg"
@@ -427,6 +459,30 @@ function DiscordIcon() {
           fill-rule="nonzero"
         />
       </g>
+    </svg>
+  )
+}
+
+function GithubIcon() {
+  return (
+    <svg
+      aria-hidden="true"
+      viewBox="0 0 98 96"
+      xmlns="http://www.w3.org/2000/svg"
+      style={{
+        position: 'absolute',
+        top: 18,
+        right: 18,
+        height: 20,
+        width: 20,
+      }}
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
+        fill="#fff"
+      />
     </svg>
   )
 }
