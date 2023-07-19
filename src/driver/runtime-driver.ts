9577bf7d067754d1dcf0ffa4e228cb5172ce176e
Sami Koskimäki <koskomi@gmail.com>
2023-07-19 09:31:17 +0300

    avoid unnecessary awaits in runtime driver

--- src/driver/runtime-driver.ts
+++ src/driver/runtime-driver.ts
@@ -14,27 +14,36 @@ export class RuntimeDriver implements Driver {
   readonly #log: Log
 
   #initPromise?: Promise<void>
+  #initDone: boolean
   #destroyPromise?: Promise<void>
   #connections = new WeakSet<DatabaseConnection>()
 
   constructor(driver: Driver, log: Log) {
+    this.#initDone = false
     this.#driver = driver
     this.#log = log
   }
 
   async init(): Promise<void> {
     if (!this.#initPromise) {
-      this.#initPromise = this.#driver.init().catch((err) => {
-        this.#initPromise = undefined
-        return Promise.reject(err)
-      })
+      this.#initPromise = this.#driver
+        .init()
+        .then(() => {
+          this.#initDone = true
+        })
+        .catch((err) => {
+          this.#initPromise = undefined
+          return Promise.reject(err)
+        })
     }
 
     await this.#initPromise
   }
 
   async acquireConnection(): Promise<DatabaseConnection> {
-    await this.init()
+    if (!this.#initDone) {
+      await this.init()
+    }
 
     const connection = await this.#driver.acquireConnection()
 
