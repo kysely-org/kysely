a9981254a9db587b22c20a1b4fee4af76b64b3b1
Sami Koskimäki <koskomi@gmail.com>
2023-07-22 06:56:34 +0300

    Support arrays in set operations. closes #287 (#606)

--- src/parser/set-operation-parser.ts
+++ src/parser/set-operation-parser.ts
@@ -1,18 +1,36 @@
+import {
+  ExpressionBuilder,
+  createExpressionBuilder,
+} from '../expression/expression-builder.js'
 import { Expression } from '../expression/expression.js'
 import {
   SetOperator,
   SetOperationNode,
 } from '../operation-node/set-operation-node.js'
-import { ExpressionFactory, parseExpression } from './expression-parser.js'
+import { isFunction, isReadonlyArray } from '../util/object-utils.js'
+import { parseExpression } from './expression-parser.js'
 
 export type SetOperandExpression<DB, O> =
   | Expression<O>
-  | ExpressionFactory<DB, never, O>
+  | ReadonlyArray<Expression<O>>
+  | ((
+      eb: ExpressionBuilder<DB, never>
+    ) => Expression<O> | ReadonlyArray<Expression<O>>)
 
-export function parseSetOperation(
+export function parseSetOperations(
   operator: SetOperator,
   expression: SetOperandExpression<any, any>,
   all: boolean
 ) {
-  return SetOperationNode.create(operator, parseExpression(expression), all)
+  if (isFunction(expression)) {
+    expression = expression(createExpressionBuilder())
+  }
+
+  if (!isReadonlyArray(expression)) {
+    expression = [expression]
+  }
+
+  return expression.map((expr) =>
+    SetOperationNode.create(operator, parseExpression(expr), all)
+  )
 }
