ebcca8bb9a3d9cff53f324bcdb583308f3f7745c
igalklebanov <igalklebanov@gmail.com>
2023-08-04 00:21:10 +0300

    introduce mssql to expression test suite.

--- test/node/src/expression.test.ts
+++ test/node/src/expression.test.ts
@@ -1,16 +1,15 @@
 import {
-  DIALECTS,
   clearDatabase,
   destroyTest,
   initTest,
   insertDefaultDataSet,
   TestContext,
   testSql,
-  NOT_SUPPORTED,
+  DIALECTS_WITH_MSSQL,
 } from './test-setup.js'
 
-for (const dialect of DIALECTS) {
-  describe(`${dialect}: expressions`, () => {
+for (const dialect of DIALECTS_WITH_MSSQL) {
+  describe.only(`${dialect}: expressions`, () => {
     let ctx: TestContext
 
     before(async function () {
@@ -70,7 +69,11 @@ for (const dialect of DIALECTS) {
                 last_name: eb.ref('first_name'),
               }),
               // Boolean literal
-              eb.lit(true),
+              ...(dialect === 'postgres' ||
+              dialect === 'mysql' ||
+              dialect === 'sqlite'
+                ? [eb.lit(true)]
+                : []),
               // Between expressions
               eb.between('id', 1000, 2000),
               ...(dialect === 'postgres'
@@ -81,7 +84,7 @@ for (const dialect of DIALECTS) {
 
       testSql(query, dialect, {
         postgres: {
-          sql: 'select "person".* from "person" where ((not "first_name" = $1 or "id" + $2 > $3 or "id" in ($4, $5, $6) or upper("first_name") = $7 or false) and exists (select "pet"."id" from "pet" where "pet"."owner_id" = "person"."id") and true and ("id" = $8 or "id" = $9 or "id" = $10 or "id" = $11) and ("id" = $12 and "first_name" = $13 and "last_name" = $14 and "marital_status" = $15) and ("id" = $16 or "id" = $17) and ("id" + $18) > $19 and ("first_name" = $20 and "last_name" = $21) and ("first_name" = "last_name" or "last_name" = "first_name") and true and "id" between $22 and $23 and "id" between symmetric $24 and $25)',
+          sql: 'select "person".* from "person" where ((not "first_name" = $1 or "id" + $2 > $3 or "id" in ($4, $5, $6) or upper("first_name") = $7 or 1 = 0) and exists (select "pet"."id" from "pet" where "pet"."owner_id" = "person"."id") and 1 = 1 and ("id" = $8 or "id" = $9 or "id" = $10 or "id" = $11) and ("id" = $12 and "first_name" = $13 and "last_name" = $14 and "marital_status" = $15) and ("id" = $16 or "id" = $17) and ("id" + $18) > $19 and ("first_name" = $20 and "last_name" = $21) and ("first_name" = "last_name" or "last_name" = "first_name") and true and "id" between $22 and $23 and "id" between symmetric $24 and $25)',
           parameters: [
             'Jennifer',
             1,
@@ -111,7 +114,7 @@ for (const dialect of DIALECTS) {
           ],
         },
         mysql: {
-          sql: 'select `person`.* from `person` where ((not `first_name` = ? or `id` + ? > ? or `id` in (?, ?, ?) or upper(`first_name`) = ? or false) and exists (select `pet`.`id` from `pet` where `pet`.`owner_id` = `person`.`id`) and true and (`id` = ? or `id` = ? or `id` = ? or `id` = ?) and (`id` = ? and `first_name` = ? and `last_name` = ? and `marital_status` = ?) and (`id` = ? or `id` = ?) and (`id` + ?) > ? and (`first_name` = ? and `last_name` = ?) and (`first_name` = `last_name` or `last_name` = `first_name`) and true and `id` between ? and ?)',
+          sql: 'select `person`.* from `person` where ((not `first_name` = ? or `id` + ? > ? or `id` in (?, ?, ?) or upper(`first_name`) = ? or 1 = 0) and exists (select `pet`.`id` from `pet` where `pet`.`owner_id` = `person`.`id`) and 1 = 1 and (`id` = ? or `id` = ? or `id` = ? or `id` = ?) and (`id` = ? and `first_name` = ? and `last_name` = ? and `marital_status` = ?) and (`id` = ? or `id` = ?) and (`id` + ?) > ? and (`first_name` = ? and `last_name` = ?) and (`first_name` = `last_name` or `last_name` = `first_name`) and true and `id` between ? and ?)',
           parameters: [
             'Jennifer',
             1,
@@ -138,9 +141,36 @@ for (const dialect of DIALECTS) {
             2000,
           ],
         },
-        mssql: NOT_SUPPORTED,
+        mssql: {
+          sql: 'select "person".* from "person" where ((not "first_name" = @1 or "id" + @2 > @3 or "id" in (@4, @5, @6) or upper("first_name") = @7 or 1 = 0) and exists (select "pet"."id" from "pet" where "pet"."owner_id" = "person"."id") and 1 = 1 and ("id" = @8 or "id" = @9 or "id" = @10 or "id" = @11) and ("id" = @12 and "first_name" = @13 and "last_name" = @14 and "marital_status" = @15) and ("id" = @16 or "id" = @17) and ("id" + @18) > @19 and ("first_name" = @20 and "last_name" = @21) and ("first_name" = "last_name" or "last_name" = "first_name") and "id" between @22 and @23)',
+          parameters: [
+            'Jennifer',
+            1,
+            10,
+            10,
+            20,
+            30,
+            'SYLVESTER',
+            1,
+            2,
+            3,
+            4,
+            1,
+            'Jennifer',
+            'Aniston',
+            'divorced',
+            1,
+            2,
+            1,
+            10,
+            'Jennifer',
+            'Aniston',
+            1000,
+            2000,
+          ],
+        },
         sqlite: {
-          sql: 'select "person".* from "person" where ((not "first_name" = ? or "id" + ? > ? or "id" in (?, ?, ?) or upper("first_name") = ? or false) and exists (select "pet"."id" from "pet" where "pet"."owner_id" = "person"."id") and true and ("id" = ? or "id" = ? or "id" = ? or "id" = ?) and ("id" = ? and "first_name" = ? and "last_name" = ? and "marital_status" = ?) and ("id" = ? or "id" = ?) and ("id" + ?) > ? and ("first_name" = ? and "last_name" = ?) and ("first_name" = "last_name" or "last_name" = "first_name") and true and "id" between ? and ?)',
+          sql: 'select "person".* from "person" where ((not "first_name" = ? or "id" + ? > ? or "id" in (?, ?, ?) or upper("first_name") = ? or 1 = 0) and exists (select "pet"."id" from "pet" where "pet"."owner_id" = "person"."id") and 1 = 1 and ("id" = ? or "id" = ? or "id" = ? or "id" = ?) and ("id" = ? and "first_name" = ? and "last_name" = ? and "marital_status" = ?) and ("id" = ? or "id" = ?) and ("id" + ?) > ? and ("first_name" = ? and "last_name" = ?) and ("first_name" = "last_name" or "last_name" = "first_name") and true and "id" between ? and ?)',
           parameters: [
             'Jennifer',
             1,
