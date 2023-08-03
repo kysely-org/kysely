554e0fb67153b1956b87f26050ffeae1e61127a9
Sami Koskimäki <koskomi@gmail.com>
2023-08-03 11:07:42 +0300

    improve relations recipe

--- test/typings/test-d/postgres-json.test-d.ts
+++ test/typings/test-d/postgres-json.test-d.ts
@@ -1,8 +1,15 @@
-import { jsonBuildObject, Kysely, jsonArrayFrom, jsonObjectFrom, sql } from '..'
+import {
+  jsonBuildObject,
+  Kysely,
+  jsonArrayFrom,
+  jsonObjectFrom,
+  sql,
+  ExpressionBuilder,
+} from '..'
 import { Database } from '../shared'
 import { expectType } from 'tsd'
 
-async function testPostgresJsonb(db: Kysely<Database>) {
+async function testPostgresJsonSelects(db: Kysely<Database>) {
   const query = db.selectFrom('person').select([
     'person.first_name',
 
@@ -48,4 +55,44 @@ async function testPostgresJsonb(db: Kysely<Database>) {
       name: { first: string; last: string | null; full: string }
     }[]
   >(r1)
+}
+
+async function testPostgresConditionalJsonSelects(db: Kysely<Database>) {
+  const query = db
+    .selectFrom('person')
+    .select(['person.first_name'])
+    .$if(Math.random() < 0.5, (qb) => qb.select(withPets))
+    .$if(Math.random() < 0.5, (qb) => qb.select(withDoggo))
+
+  const r1 = await query.execute()
+
+  expectType<
+    {
+      first_name: string
+      pets?: { name: string; species: 'dog' | 'cat' }[]
+      doggo?: { doggo_name: string } | null
+    }[]
+  >(r1)
+}
+
+function withPets(eb: ExpressionBuilder<Database, 'person'>) {
+  return jsonArrayFrom(
+    eb
+      .selectFrom('pet')
+      .select(['name', 'species'])
+      .whereRef('owner_id', '=', 'person.id')
+      .orderBy('pet.name')
+  ).as('pets')
+}
+
+function withDoggo(eb: ExpressionBuilder<Database, 'person'>) {
+  return jsonObjectFrom(
+    eb
+      .selectFrom('pet')
+      .select('name as doggo_name')
+      .whereRef('owner_id', '=', 'person.id')
+      .where('species', '=', 'dog')
+      .orderBy('name')
+      .limit(1)
+  ).as('doggo')
 }
