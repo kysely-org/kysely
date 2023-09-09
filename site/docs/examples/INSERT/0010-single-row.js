580835e7d1188f2715a32b21af4dfa00d44ce9af
Sami Koskimäki <koskomi@gmail.com>
2023-09-09 11:26:06 +0300

    make example comment fit inside kysely.dev playground

--- site/docs/examples/INSERT/0010-single-row.js
+++ site/docs/examples/INSERT/0010-single-row.js
@@ -7,9 +7,9 @@ export const singleRow = `const result = await db
   })
   .executeTakeFirst()
 
-// The \`insertId\` is only available on dialects that automatically
-// return the id of the inserted row such as MySQL and SQLite.
-// On postgres, for example, you need to add a \`returning\` clause
-// to the query to get anything out. See the "returning data"
-// example.
+// The \`insertId\` is only available on dialects
+// that automatically return the id of the inserted
+// row such as MySQL and SQLite. On postgres, for example,
+// you need to add a \`returning\` clause to the query to
+// get anything out. See the "returning data" example.
 console.log(result.insertId)`
--- src/query-builder/insert-query-builder.ts
+++ src/query-builder/insert-query-builder.ts
@@ -111,11 +111,11 @@ export class InsertQueryBuilder<DB, TB extends keyof DB, O>
    *   })
    *   .executeTakeFirst()
    *
-   * // The `insertId` is only available on dialects that automatically
-   * // return the id of the inserted row such as MySQL and SQLite.
-   * // On postgres, for example, you need to add a `returning` clause
-   * // to the query to get anything out. See the "returning data"
-   * // example.
+   * // The `insertId` is only available on dialects
+   * // that automatically return the id of the inserted
+   * // row such as MySQL and SQLite. On postgres, for example,
+   * // you need to add a `returning` clause to the query to
+   * // get anything out. See the "returning data" example.
    * console.log(result.insertId)
    * ```
    *
