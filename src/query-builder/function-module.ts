3a9d03fb594a662ca694eae6c8cfd6540909fb33
Sami Koskimäki <koskomi@gmail.com>
2023-09-28 08:26:30 +0300

    improve select function calls example even more

--- src/query-builder/function-module.ts
+++ src/query-builder/function-module.ts
@@ -45,21 +45,31 @@ import { Selectable } from '../util/column-type.js'
  *     // functions.
  *     fn.count<number>('pet.id').as('pet_count'),
  *
- *     // You can call any function by calling `fn` directly.
- *     // The arguments are treated as column references by
- *     // default. If you want  to pass in values, use the
- *     // `val` function.
- *     fn<string>('concat', [val('Ms. '), 'first_name', 'last_name']).as('full_name_with_title'),
+ *     // You can call any function by calling `fn`
+ *     // directly. The arguments are treated as column
+ *     // references by default. If you want  to pass in
+ *     // values, use the `val` function.
+ *     fn<string>('concat', [
+ *       val('Ms. '),
+ *       'first_name',
+ *       val(' '),
+ *       'last_name'
+ *     ]).as('full_name_with_title'),
  *
  *     // You can call any aggregate function using the
  *     // `fn.agg` function.
  *     fn.agg<string[]>('array_agg', ['pet.name']).as('pet_names'),
  *
- *     // And once again, you can use the `sql` template tag.
- *     // The template tag substitutions are treated as values
- *     // by default. If you want to reference columns, you can
- *     // use the `ref` function.
- *     sql<string>`concat(${ref('first_name')}, ${ref('last_name')})`.as('full_name')
+ *     // And once again, you can use the `sql`
+ *     // template tag. The template tag substitutions
+ *     // are treated as values by default. If you want
+ *     // to reference columns, you can use the `ref`
+ *     // function.
+ *     sql<string>`concat(
+ *       ${ref('first_name')},
+ *       ' ',
+ *       ${ref('last_name')}
+ *     )`.as('full_name')
  *   ])
  *   .groupBy('person.id')
  *   .having((eb) => eb.fn.count('pet.id'), '>', 10)
@@ -72,13 +82,13 @@ import { Selectable } from '../util/column-type.js'
  * select
  *   "person"."id",
  *   count("pet"."id") as "pet_count",
- *   concat($1, "first_name", "last_name") as "full_name_with_title",
+ *   concat($1, "first_name", $2, "last_name") as "full_name_with_title",
  *   array_agg("pet"."name") as "pet_names",
- *   concat("first_name", "last_name") as "full_name"
+ *   concat("first_name", ' ', "last_name") as "full_name"
  * from "person"
  * inner join "pet" on "pet"."owner_id" = "person"."id"
  * group by "person"."id"
- * having count("pet"."id") > $2
+ * having count("pet"."id") > $3
  * ```
  */
 export interface FunctionModule<DB, TB extends keyof DB> {
