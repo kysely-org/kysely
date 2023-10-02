1f3fcee3f8ce8ef8be709b0032367e0dfa9db004
gaspero <gasp-gasp@mail.ru>
2023-10-02 16:27:16 +0300

    add AddIndexNode

---
+++ src/operation-node/add-index-node.ts
@@ -0,0 +1,45 @@
+import { freeze } from '../util/object-utils.js'
+import { IdentifierNode } from './identifier-node.js'
+import { OperationNode } from './operation-node.js'
+import { RawNode } from './raw-node.js'
+
+export type AddIndexNodeProps = Omit<AddIndexNode, 'kind' | 'name'>
+
+export interface AddIndexNode extends OperationNode {
+  readonly kind: 'AddIndexNode'
+  readonly name: IdentifierNode
+  readonly columns?: OperationNode[]
+  readonly unique?: boolean
+  readonly using?: RawNode
+  readonly ifNotExists?: boolean
+}
+
+/**
+ * @internal
+ */
+export const AddIndexNode = freeze({
+  is(node: OperationNode): node is AddIndexNode {
+    return node.kind === 'AddIndexNode'
+  },
+
+  create(index: Omit<AddIndexNode, 'kind'>): AddIndexNode {
+    return freeze({
+      kind: 'AddIndexNode',
+      ...index,
+    })
+  },
+
+  cloneWith(node: AddIndexNode, props: AddIndexNodeProps): AddIndexNode {
+    return freeze({
+      ...node,
+      ...props,
+    })
+  },
+
+  cloneWithColumns(node: AddIndexNode, columns: OperationNode[]): AddIndexNode {
+    return freeze({
+      ...node,
+      columns: [...(node.columns || []), ...columns],
+    })
+  },
+})
