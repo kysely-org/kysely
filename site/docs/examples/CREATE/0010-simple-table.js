cccae7f62d982ba8ee756823b6489938b097be48
Alex Vershinin <alexander.vershinin@quantori.com>
2023-11-19 19:41:35 +0400

    chore: reverted site changes

--- site/docs/examples/CREATE/0010-simple-table.js
+++
@@ -1,7 +0,0 @@
-export const createTable = `await db.schema
-  .createTable("person")
-  .addColumn("id", "serial", (cb) => cb.primaryKey())
-  .addColumn("first_name", "varchar", (cb) => cb.notNull())
-  .addColumn("last_name", "varchar")
-  .addColumn("gender", "varchar(10)", (cb) => cb.notNull())
-  .execute();`
