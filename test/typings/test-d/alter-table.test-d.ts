3d7cad2118468f16cdd455a501898e0309be3b12
Alex Vershinin <alexander.vershinin@quantori.com>
2023-11-20 01:14:33 +0400

    test: added typing checks for alter table and nullsNotDistinct

---
+++ test/typings/test-d/alter-table.test-d.ts
@@ -0,0 +1,21 @@
+import { expectError } from 'tsd'
+import { Kysely } from '..'
+import { Database } from '../shared'
+
+async function testAlterTableWithAddUniqueConstraint(db: Kysely<Database>) {
+  expectError(
+    db.schema
+      .alterTable('test')
+      .addUniqueConstraint(null, ['a'], (uc) => uc.nullsNotDistinct())
+  )
+  expectError(
+    db.schema
+      .alterTable('test')
+      .addUniqueConstraint('a_unique', [1], (uc) => uc.nullsNotDistinct())
+  )
+  expectError(
+    db.schema
+      .alterTable('test')
+      .addUniqueConstraint('a_unique', ['a'], 'wrong option')
+  )
+}
