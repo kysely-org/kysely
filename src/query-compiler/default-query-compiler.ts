4b1401429cfd52757eed1fbc34e1327e24b44f7b
Markus Wendorf <markuswendorf@web.de>
2024-03-10 09:25:07 +0100

    feat: add 'add column if not exists' for postgres (#900)

--- src/query-compiler/default-query-compiler.ts
+++ src/query-compiler/default-query-compiler.ts
@@ -607,6 +607,10 @@ export class DefaultQueryCompiler
   }
 
   protected override visitColumnDefinition(node: ColumnDefinitionNode): void {
+    if (node.ifNotExists) {
+      this.append('if not exists ')
+    }
+
     this.visitNode(node.column)
 
     this.append(' ')
