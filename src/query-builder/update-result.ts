1e9190262bb0c28a9eb46ce1dd9a3ac519e388f2
Shahab Movahhedi <45717968+movahhedi@users.noreply.github.com>
2024-05-18 18:37:56 +0330

    docs: add comments for `UpdateResult`

--- src/query-builder/update-result.ts
+++ src/query-builder/update-result.ts
@@ -1,5 +1,14 @@
 export class UpdateResult {
+  /**
+   * The number of rows the update query updated.
+  */
   readonly numUpdatedRows: bigint
+
+  /**
+   * The number of rows the update query changed.
+   * This is **optional** and only provided by some drivers like `node-mysql2`.
+   * You would probably use `numUpdatedRows` in most cases.
+  */
   readonly numChangedRows?: bigint
 
   constructor(numUpdatedRows: bigint, numChangedRows: bigint | undefined) {
