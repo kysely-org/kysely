9e01281af2d16ad0b23c2d08291e9267dd98c412
Sami Koskimäki <koskomi@gmail.com>
2024-07-22 15:18:49 +0300

    add reusable helpers recipe and implement missing expression features (#1085)
    * add reusable helpers recipe and implement missing expression features
    
    * force node 22.4.1 in CI because of an npm bug

--- src/query-builder/select-query-builder.ts
+++ src/query-builder/select-query-builder.ts
@@ -243,7 +243,7 @@ export interface SelectQueryBuilder<DB, TB extends keyof DB, O>
    * import { sql } from 'kysely'
    *
    * const persons = await db.selectFrom('person')
-   *   .select(({ eb, selectFrom, or }) => [
+   *   .select(({ eb, selectFrom, or, val, lit }) => [
    *     // Select a correlated subquery
    *     selectFrom('pet')
    *       .whereRef('person.id', '=', 'pet.owner_id')
@@ -260,7 +260,13 @@ export interface SelectQueryBuilder<DB, TB extends keyof DB, O>
    *     ]).as('is_jennifer_or_arnold'),
    *
    *     // Select a raw sql expression
-   *     sql<string>`concat(first_name, ' ', last_name)`.as('full_name')
+   *     sql<string>`concat(first_name, ' ', last_name)`.as('full_name').
+   *
+   *     // Select a static string value
+   *     val('Some value').as('string_value'),
+   *
+   *     // Select a literal value
+   *     lit(42).as('literal_value'),
    *   ])
    *   .execute()
    * ```
@@ -277,7 +283,9 @@ export interface SelectQueryBuilder<DB, TB extends keyof DB, O>
    *     limit $1
    *   ) as "pet_name",
    *   ("first_name" = $2 or "first_name" = $3) as "jennifer_or_arnold",
-   *   concat(first_name, ' ', last_name) as "full_name"
+   *   concat(first_name, ' ', last_name) as "full_name",
+   *   $4 as "string_value",
+   *   42 as "literal_value"
    * from "person"
    * ```
    *
@@ -1929,6 +1937,54 @@ export interface SelectQueryBuilder<DB, TB extends keyof DB, O>
     : KyselyTypeError<'$asTuple() call failed: All selected columns must be provided as arguments'>
 
   /**
+   * Plucks the value type of the output record.
+   *
+   * In SQL, any record type that only has one column can be used as a scalar.
+   * For example a query like this works:
+   *
+   * ```sql
+   * select
+   *   id,
+   *   first_name
+   * from
+   *   person as p
+   * where
+   *   -- This is ok since the query only selects one row
+   *   -- and one column.
+   *  (select name from pet where pet.owner_id = p.id limit 1) = 'Doggo'
+   * ```
+   *
+   * In many cases Kysely handles this automatically and picks the correct
+   * scalar type instead of the record type, but sometimes you need to give
+   * Kysely a hint.
+   *
+   * One such case are custom helper functions that take `Expression<T>`
+   * instances as inputs:
+   *
+   * ```ts
+   * function doStuff(expr: Expression<string>) {
+   *   ...
+   * }
+   *
+   * // Error! This is not ok because the expression type is
+   * // `{ first_name: string }` instead of `string`.
+   * doStuff(db.selectFrom('person').select('first_name'))
+   *
+   * // Ok! This is ok since we've plucked the `string` type of the
+   * // only column in the output type.
+   * doStuff(db.selectFrom('person').select('first_name').$asScalar())
+   * ```
+   *
+   * This function has absolutely no effect on the generated SQL. It's
+   * purely a type-level helper.
+   *
+   * This method returns an `ExpressionWrapper` instead of a `SelectQueryBuilder`
+   * since the return value should only be used as a part of an expression
+   * and never executed as the main query.
+   */
+  $asScalar<K extends keyof O = keyof O>(): ExpressionWrapper<DB, TB, O[K]>
+
+  /**
    * Narrows (parts of) the output type of the query.
    *
    * Kysely tries to be as type-safe as possible, but in some cases we have to make
@@ -2558,6 +2614,10 @@ class SelectQueryBuilderImpl<DB, TB extends keyof DB, O>
   }
 
   $asTuple(): ExpressionWrapper<DB, TB, any> {
+    return new ExpressionWrapper(this.toOperationNode())
+  }
+
+  $asScalar(): ExpressionWrapper<DB, TB, any> {
     return new ExpressionWrapper(this.toOperationNode())
   }
 
