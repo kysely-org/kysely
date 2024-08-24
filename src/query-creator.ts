f7dcc5d23546cdedd7bbb9e1e672e757e1159cec
Kristinn Thor Johannsson <ktj@skalar.no>
2024-08-24 20:47:28 +0200

    docs: updated outdated links
    some links were pointing to what I'm assuming is the previous repository for kysely

--- src/query-creator.ts
+++ src/query-creator.ts
@@ -738,7 +738,7 @@ export class QueryCreator<DB> {
    * This only affects the query created through the builder returned from
    * this method and doesn't modify the `db` instance.
    *
-   * See [this recipe](https://github.com/koskimas/kysely/tree/master/site/docs/recipes/schemas.md)
+   * See [this recipe](https://github.com/kysely-org/kysely/blob/master/site/docs/recipes/0007-schemas.md)
    * for a more detailed explanation.
    *
    * ### Examples
