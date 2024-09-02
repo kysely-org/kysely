a7abe0f95933b25ec21f38296f4c0a0930dec7bf
igalklebanov <igalklebanov@gmail.com>
2024-09-02 12:22:45 +0300

    add `appendSerializedValue`.

--- src/query-compiler/default-query-compiler.ts
+++ src/query-compiler/default-query-compiler.ts
@@ -502,12 +502,10 @@ export class DefaultQueryCompiler
   protected override visitValue(node: ValueNode): void {
     if (node.immediate) {
       this.appendImmediateValue(node.value)
+    } else if (node.serialized) {
+      this.appendSerializedValue(node.value)
     } else {
-      this.appendValue(
-        node.serialized && node.value !== null
-          ? JSON.stringify(node.value)
-          : node.value,
-      )
+      this.appendValue(node.value)
     }
   }
 
@@ -1669,6 +1667,14 @@ export class DefaultQueryCompiler
   protected appendValue(parameter: unknown): void {
     this.addParameter(parameter)
     this.append(this.getCurrentParameterPlaceholder())
+  }
+
+  protected appendSerializedValue(parameter: unknown): void {
+    if (parameter === null) {
+      this.appendValue(null)
+    } else {
+      this.appendValue(JSON.stringify(parameter))
+    }
   }
 
   protected getLeftIdentifierWrapper(): string {
