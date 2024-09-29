d00f49c73c902f5842a9b0f9188e5e90484ea2ef
wirekang <wirekang@gmail.com>
2024-09-29 22:41:57 +0900

    Remove preventAwait (#1160)
    Related #748

--- src/schema/create-type-builder.ts
+++ src/schema/create-type-builder.ts
@@ -1,7 +1,6 @@
 import { OperationNodeSource } from '../operation-node/operation-node-source.js'
 import { CompiledQuery } from '../query-compiler/compiled-query.js'
 import { Compilable } from '../util/compilable.js'
-import { preventAwait } from '../util/prevent-await.js'
 import { QueryExecutor } from '../query-executor/query-executor.js'
 import { QueryId } from '../util/query-id.js'
 import { freeze } from '../util/object-utils.js'
@@ -56,11 +55,6 @@ export class CreateTypeBuilder implements OperationNodeSource, Compilable {
     await this.#props.executor.executeQuery(this.compile(), this.#props.queryId)
   }
 }
-
-preventAwait(
-  CreateTypeBuilder,
-  "don't await CreateTypeBuilder instances directly. To execute the query you need to call `execute`",
-)
 
 export interface CreateTypeBuilderProps {
   readonly queryId: QueryId
