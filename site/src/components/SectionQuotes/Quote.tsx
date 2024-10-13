fe1b57608c9bf0155a7f51fbef7c79f89794d631
igalklebanov <igalklebanov@gmail.com>
2024-10-13 22:28:23 +0300

    docs: refresh quotes.

--- site/src/components/SectionQuotes/Quote.tsx
+++ site/src/components/SectionQuotes/Quote.tsx
@@ -12,19 +12,20 @@ export interface QuoteProps {
 }
 
 const domainNameToIcon: Record<string, ReactElement> = {
-  twitter: <TwitterIcon />,
   discord: <DiscordIcon />,
   github: <GithubIcon />,
+  x: <XIcon />,
 }
 
 export function Quote(props: QuoteProps) {
   const { authorName, authorTitle, avatar, link, text } = props
 
-  const [, domainName] = link.toLowerCase().match(/https?:\/\/(?:www\.)?(.+)\./)
+  const [domainName] = new URL(link).hostname.split('.')
+
+  console.log(domainName)
 
   return (
     <a className={styles.quoteContainer} href={link} target="_blank">
-      {domainNameToIcon[domainName] || null}
       <div className={styles.quoteInnerContainer}>
         <img
           alt={`${authorName}'s avatar picture`}
@@ -38,20 +39,21 @@ export function Quote(props: QuoteProps) {
       </div>
 
       <small className={styles.quoteText}>{text}</small>
+      {domainNameToIcon[domainName] || null}
     </a>
   )
 }
 
-function TwitterIcon() {
+function XIcon() {
   return (
     <svg
-      aria-hidden="true"
-      className={clsx(styles.icon, styles.twitterIcon)}
-      viewBox="0 0 24 24"
+      className={clsx(styles.icon, styles.darkIcon)}
+      height="271"
+      viewBox="0 0 271 300"
+      width="300"
+      xmlns="http://www.w3.org/2000/svg"
     >
-      <g>
-        <path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z" />
-      </g>
+      <path d="m236 0h46l-101 115 118 156h-92.6l-72.5-94.8-83 94.8h-46l107-123-113-148h94.9l65.5 86.6zm-16.1 244h25.5l-165-218h-27.4z" />
     </svg>
   )
 }
@@ -81,15 +83,16 @@ function GithubIcon() {
   return (
     <svg
       aria-hidden="true"
-      className={styles.icon}
-      viewBox="0 0 98 96"
+      className={clsx(styles.icon, styles.darkIcon)}
+      width="98"
+      height="96"
       xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 98 96"
     >
       <path
         fill-rule="evenodd"
         clip-rule="evenodd"
         d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
-        fill="#fff"
       />
     </svg>
   )
