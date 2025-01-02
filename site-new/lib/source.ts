f7f15d7a208ba2e1c7b8fc9896c1758e41e124db
Johan Eliasson <johan@eliasson.me>
2025-01-02 21:17:29 +0100

    wip

--- site-new/lib/source.ts
+++ site-new/lib/source.ts
@@ -1,8 +1,14 @@
-import { docs, meta } from '@/.source';
-import { createMDXSource } from 'fumadocs-mdx';
-import { loader } from 'fumadocs-core/source';
+import { createElement } from 'react'
+import { docs, meta } from '@/.source'
+import { createMDXSource } from 'fumadocs-mdx'
+import { loader } from 'fumadocs-core/source'
+import { icons } from 'lucide-react'
 
 export const source = loader({
   baseUrl: '/docs',
   source: createMDXSource(docs, meta),
-});
+  icon(icon) {
+    if (icon && icon in icons)
+      return createElement(icons[icon as keyof typeof icons])
+  },
+})
--- site-new/source.config.ts
+++ site-new/source.config.ts
@@ -1,7 +1,12 @@
-import { defineDocs, defineConfig } from 'fumadocs-mdx/config';
+import { defineDocs, defineConfig } from 'fumadocs-mdx/config'
+import { remarkInstall } from 'fumadocs-docgen'
 
 export const { docs, meta } = defineDocs({
   dir: 'content/docs',
-});
+})
 
-export default defineConfig();
+export default defineConfig({
+  mdxOptions: {
+    remarkPlugins: [remarkInstall],
+  },
+})
