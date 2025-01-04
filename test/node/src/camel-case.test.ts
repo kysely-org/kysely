383e1a5da41bc6b485025f2f484b962a62b483f7
Ryan Vieira <ryanvsouza99@gmail.com>
2025-01-04 12:49:39 -0300

    fix: implements a underscore validation to camelcase plugin (#1290)
    Co-authored-by: Ryan Vieira  <53231910+RyanV-Souza@users.noreply.github.com>
    Co-authored-by: Igal Klebanov <igalklebanov@gmail.com>

--- test/node/src/camel-case.test.ts
+++ test/node/src/camel-case.test.ts
@@ -30,6 +30,7 @@ for (const dialect of DIALECTS) {
       preferences: {
         disable_emails: boolean
       }
+      addressRow1: string
     }
 
     interface CamelDatabase {
@@ -52,6 +53,7 @@ for (const dialect of DIALECTS) {
           'preferences',
           dialect === 'mssql' ? 'varchar(8000)' : 'json',
         )
+        .addColumn('addressRow1', 'varchar(255)')
         .execute()
     })
 
@@ -63,11 +65,13 @@ for (const dialect of DIALECTS) {
             firstName: 'Jennifer',
             lastName: 'Aniston',
             preferences: json({ disable_emails: true }),
+            addressRow1: '123 Main St',
           },
           {
             firstName: 'Arnold',
             lastName: 'Schwarzenegger',
             preferences: json({ disable_emails: true }),
+            addressRow1: '123 Main St',
           },
         ])
         .execute()
@@ -306,6 +310,48 @@ for (const dialect of DIALECTS) {
 
       expect(data.preferences).to.eql({
         disable_emails: true,
+      })
+    })
+
+    it.only('should respect `underscoreBeforeDigits` and not add a second underscore in a nested query', async () => {
+      const db = camelDb
+        .withoutPlugins()
+        .withPlugin(new CamelCasePlugin({ underscoreBeforeDigits: true }))
+
+      const query = db
+        .selectFrom(
+          db
+            .selectFrom('camelPerson')
+            .select('addressRow1')
+            .as('originalQuery'),
+        )
+        .selectAll()
+
+      testSql(query, dialect, {
+        postgres: {
+          sql: [
+            `select * from (select "address_row_1" from "camel_person") as "original_query"`,
+          ],
+          parameters: [],
+        },
+        mysql: {
+          sql: [
+            'select * from (select `address_row_1` from `camel_person`) as `original_query`',
+          ],
+          parameters: [],
+        },
+        mssql: {
+          sql: [
+            `select * from (select "address_row_1" from "camel_person") as "original_query"`,
+          ],
+          parameters: [],
+        },
+        sqlite: {
+          sql: [
+            `select * from (select "address_row_1" from "camel_person") as "original_query"`,
+          ],
+          parameters: [],
+        },
       })
     })
 
