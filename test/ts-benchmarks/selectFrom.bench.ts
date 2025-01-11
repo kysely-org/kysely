cef96863c664201203f40c96df9c710c69b448b6
igalklebanov <igalklebanov@gmail.com>
2025-01-11 14:05:37 +0200

    update bench numbers.

--- test/ts-benchmarks/selectFrom.bench.ts
+++ test/ts-benchmarks/selectFrom.bench.ts
@@ -3,93 +3,96 @@ import type { DB } from '../typings/test-d/huge-db.test-d'
 import type { Kysely } from '../../dist/esm/index.js'
 
 declare const kysely: Kysely<DB>
-declare const knex: Kysely<any>
+declare const kyselyAny: Kysely<any>
 
 bench.baseline(() => {})
 
 bench('kysely.selectFrom(table)', () => {
   return kysely.selectFrom('table_fff4c6195261874920bc7ce92d67d2c2')
-}).types([388, 'instantiations'])
+}).types([372, 'instantiations'])
 
 bench('kysely.selectFrom(~table)', () => {
   return kysely.selectFrom('my_table2')
-}).types([9314, 'instantiations'])
+}).types([6864, 'instantiations'])
 
 bench('kysely.selectFrom(table as alias)', () => {
   return kysely.selectFrom('my_table as mt')
-}).types([401, 'instantiations'])
+}).types([385, 'instantiations'])
 
 bench('kysely.selectFrom([table])', () => {
   return kysely.selectFrom(['my_table'])
-}).types([413, 'instantiations'])
+}).types([427, 'instantiations'])
 
 bench('kysely.selectFrom([~table])', () => {
   return kysely.selectFrom(['my_table2'])
-}).types([9364, 'instantiations'])
+}).types([6914, 'instantiations'])
 
 bench('kysely.selectFrom([table as alias])', () => {
   return kysely.selectFrom(['my_table as mt'])
-}).types([413, 'instantiations'])
+}).types([427, 'instantiations'])
 
 bench('kysely.selectFrom([table, table])', () => {
   return kysely.selectFrom([
     'my_table',
     'table_000a8a0cb7f265a624c851d3e7f8b946',
   ])
-}).types([413, 'instantiations'])
+}).types([427, 'instantiations'])
 
 bench('kysely.selectFrom([table, ~table])', () => {
   return kysely.selectFrom([
     'my_table',
     'table_000a8a0cb7f265a624c851d3e7f8b9462',
   ])
-}).types([9367, 'instantiations'])
+}).types([6917, 'instantiations'])
 
 bench('kysely.selectFrom([table as alias, table as alias])', () => {
   return kysely.selectFrom([
     'my_table as mt',
     'table_000a8a0cb7f265a624c851d3e7f8b946 as t',
   ])
-}).types([413, 'instantiations'])
+}).types([427, 'instantiations'])
 
-bench('knex.selectFrom(table)', () => {
-  return knex.selectFrom('table_fff4c6195261874920bc7ce92d67d2c2')
-}).types([140, 'instantiations'])
+bench('kyselyAny.selectFrom(table)', () => {
+  return kyselyAny.selectFrom('table_fff4c6195261874920bc7ce92d67d2c2')
+}).types([124, 'instantiations'])
 
-bench('knex.selectFrom(~table)', () => {
-  return knex.selectFrom('my_table2')
-}).types([140, 'instantiations'])
+bench('kyselyAny.selectFrom(~table)', () => {
+  return kyselyAny.selectFrom('my_table2')
+}).types([124, 'instantiations'])
 
-bench('knex.selectFrom(table as alias)', () => {
-  return knex.selectFrom('my_table as mt')
-}).types([140, 'instantiations'])
+bench('kyselyAny.selectFrom(table as alias)', () => {
+  return kyselyAny.selectFrom('my_table as mt')
+}).types([124, 'instantiations'])
 
-bench('knex.selectFrom([table])', () => {
-  return knex.selectFrom(['my_table'])
+bench('kyselyAny.selectFrom([table])', () => {
+  return kyselyAny.selectFrom(['my_table'])
 }).types([165, 'instantiations'])
 
-bench('knex.selectFrom([~table])', () => {
-  return knex.selectFrom(['my_table2'])
-}).types([165, 'instantiations'])
+bench('kyselyAny.selectFrom([~table])', () => {
+  return kyselyAny.selectFrom(['my_table2'])
+}).types([179, 'instantiations'])
 
-bench('knex.selectFrom([table as alias])', () => {
-  return knex.selectFrom(['my_table as mt'])
-}).types([165, 'instantiations'])
+bench('kyselyAny.selectFrom([table as alias])', () => {
+  return kyselyAny.selectFrom(['my_table as mt'])
+}).types([179, 'instantiations'])
 
-bench('knex.selectFrom([table, table])', () => {
-  return knex.selectFrom(['my_table', 'table_000a8a0cb7f265a624c851d3e7f8b946'])
-}).types([165, 'instantiations'])
+bench('kyselyAny.selectFrom([table, table])', () => {
+  return kyselyAny.selectFrom([
+    'my_table',
+    'table_000a8a0cb7f265a624c851d3e7f8b946',
+  ])
+}).types([179, 'instantiations'])
 
-bench('knex.selectFrom([table, ~table])', () => {
-  return knex.selectFrom([
+bench('kyselyAny.selectFrom([table, ~table])', () => {
+  return kyselyAny.selectFrom([
     'my_table',
     'table_000a8a0cb7f265a624c851d3e7f8b9462',
   ])
-}).types([165, 'instantiations'])
+}).types([179, 'instantiations'])
 
-bench('knex.selectFrom([table as alias, table as alias])', () => {
-  return knex.selectFrom([
+bench('kyselyAny.selectFrom([table as alias, table as alias])', () => {
+  return kyselyAny.selectFrom([
     'my_table as mt',
     'table_000a8a0cb7f265a624c851d3e7f8b946 as t',
   ])
-}).types([165, 'instantiations'])
+}).types([179, 'instantiations'])
