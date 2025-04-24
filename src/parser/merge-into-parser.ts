a5d4dd8add8e4e66a52601e7f3402d8d6efe0bfc
Sami Koskimäki <koskomi@gmail.com>
2025-04-24 20:54:34 +0300

    fix #1440

--- src/parser/merge-into-parser.ts
+++ src/parser/merge-into-parser.ts
@@ -3,12 +3,11 @@ import type { MergeResult } from '../query-builder/merge-result.js'
 import type { ShallowRecord } from '../util/type-utils.js'
 import type { ExtractTableAlias, SimpleTableReference } from './table-parser.js'
 
-export type MergeInto<
-  DB,
-  TE extends SimpleTableReference<DB>,
-> = TE extends keyof DB & string
+export type MergeInto<DB, TE extends SimpleTableReference<DB>> = [TE] extends [
+  keyof DB,
+]
   ? MergeQueryBuilder<DB, ExtractTableAlias<DB, TE>, MergeResult>
-  : TE extends `${infer T} as ${infer A}`
+  : [TE] extends [`${infer T} as ${infer A}`]
     ? T extends keyof DB
       ? MergeQueryBuilder<DB & ShallowRecord<A, DB[T]>, A, MergeResult>
       : never
--- src/parser/update-parser.ts
+++ src/parser/update-parser.ts
@@ -8,34 +8,35 @@ import type {
   TableExpressionOrList,
 } from './table-parser.js'
 
-export type UpdateTable<DB, TE extends TableExpressionOrList<DB, never>> =
-  TE extends ReadonlyArray<infer T>
-    ? UpdateQueryBuilder<
-        From<DB, T>,
-        FromTables<DB, never, T>,
-        FromTables<DB, never, T>,
-        UpdateResult
-      >
-    : TE extends keyof DB & string
-      ? // This branch creates a good-looking type for the most common case:
-        // updateTable('person') --> UpdateQueryBuilder<DB, 'person', 'person', {}>.
-        // ExtractTableAlias is needed for the case where DB == any. Without it:
-        // updateTable('person as p') --> UpdateQueryBuilder<DB, 'person as p', 'person as p', {}>
-        UpdateQueryBuilder<
-          DB,
-          ExtractTableAlias<DB, TE>,
-          ExtractTableAlias<DB, TE>,
+export type UpdateTable<DB, TE extends TableExpressionOrList<DB, never>> = [
+  TE,
+] extends [keyof DB]
+  ? // This branch creates a good-looking type for the most common case:
+    // updateTable('person') --> UpdateQueryBuilder<DB, 'person', 'person', {}>.
+    // ExtractTableAlias is needed for the case where DB == any. Without it:
+    // updateTable('person as p') --> UpdateQueryBuilder<DB, 'person as p', 'person as p', {}>
+    UpdateQueryBuilder<
+      DB,
+      ExtractTableAlias<DB, TE>,
+      ExtractTableAlias<DB, TE>,
+      UpdateResult
+    >
+  : // This branch creates a good-looking type for common aliased case:
+    // updateTable('person as p') --> UpdateQueryBuilder<DB & { p: Person }, 'p', 'p', {}>.
+    [TE] extends [`${infer T} as ${infer A}`]
+    ? T extends keyof DB
+      ? UpdateQueryBuilder<DB & ShallowRecord<A, DB[T]>, A, A, UpdateResult>
+      : never
+    : TE extends ReadonlyArray<infer T>
+      ? UpdateQueryBuilder<
+          From<DB, T>,
+          FromTables<DB, never, T>,
+          FromTables<DB, never, T>,
           UpdateResult
         >
-      : // This branch creates a good-looking type for common aliased case:
-        // updateTable('person as p') --> UpdateQueryBuilder<DB & { p: Person }, 'p', 'p', {}>.
-        TE extends `${infer T} as ${infer A}`
-        ? T extends keyof DB
-          ? UpdateQueryBuilder<DB & ShallowRecord<A, DB[T]>, A, A, UpdateResult>
-          : never
-        : UpdateQueryBuilder<
-            From<DB, TE>,
-            FromTables<DB, never, TE>,
-            FromTables<DB, never, TE>,
-            UpdateResult
-          >
+      : UpdateQueryBuilder<
+          From<DB, TE>,
+          FromTables<DB, never, TE>,
+          FromTables<DB, never, TE>,
+          UpdateResult
+        >
