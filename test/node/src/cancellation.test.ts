b3efdb26a19a5f7802ade48b61789bfb8c4d787d
igalklebanov <igalklebanov@gmail.com>
2025-07-05 05:08:08 +0300

    ...

--- test/node/src/cancellation.test.ts
+++ test/node/src/cancellation.test.ts
@@ -58,17 +58,21 @@ for (const dialect of DIALECTS) {
       })
 
       it('should throw an abort error when aborted during query execution', async () => {
-        const sleepQuery = (
+        const writeQuery = ctx.db
+          .insertInto('person')
+          .values({ gender: sql.lit('woah') as never })
+
+        const delayedQuery = (
           {
-            postgres: sql`select pg_sleep(300)`,
-            mysql: sql`select sleep(300)`,
-            mssql: sql`waitfor delay '00:05:00.000'`,
+            postgres: sql`select pg_sleep(1); ${writeQuery};`,
+            mysql: sql`select sleep(1); ${writeQuery};`,
+            mssql: sql`waitfor delay '00:00:01.000'; ${writeQuery};`,
             sqlite: sql`WITH RECURSIVE timer(i) AS (
               SELECT 1
               UNION ALL
               SELECT i + 1 FROM timer WHERE i < 10000000
             )
-            SELECT COUNT(*) FROM timer;`,
+            SELECT COUNT(*) FROM timer; ${writeQuery};`,
           } as const satisfies Record<BuiltInDialect, RawBuilder<any>>
         )[dialect]
 
@@ -76,13 +80,24 @@ for (const dialect of DIALECTS) {
         setTimeout(10).then(() => abortController.abort())
 
         await expect(
-          sleepQuery.execute(ctx.db, { abortSignal: abortController.signal }),
+          delayedQuery.execute(ctx.db, { abortSignal: abortController.signal }),
         )
           .to.eventually.be.rejectedWith(AbortError)
           .and.satisfies(
             (error: AbortError) =>
               error.reason === 'aborted during query execution',
           )
+
+        // this checks the write after the sleep query was not executed due to the abort.
+        await expect(
+          ctx.db
+            .selectFrom('person')
+            .where('gender', '=', 'woah' as never)
+            .select(ctx.db.fn.countAll().as('count'))
+            .executeTakeFirstOrThrow(),
+        ).to.eventually.satisfy(
+          (result: { count: unknown }) => Number(result.count) === 0,
+        )
       })
 
       it('should throw an abort error when aborted during result transformation', async () => {
