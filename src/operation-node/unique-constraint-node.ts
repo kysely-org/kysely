9f00d136bb576a0759437b7515d12025b5fa7a76
Eric So <56284867+ericsodev@users.noreply.github.com>
2025-07-26 18:55:49 -0400

    feat: allow expressions in unique constraint (#1518)
    Co-authored-by: Eric So <56284867+ericsodev@users.noreply.github.com>
    Co-authored-by: Igal Klebanov <igalklebanov@gmail.com>

--- src/operation-node/unique-constraint-node.ts
+++ src/operation-node/unique-constraint-node.ts
@@ -1,11 +1,12 @@
-import { freeze } from '../util/object-utils.js'
+import { logOnce } from '../util/log-once.js'
+import { freeze, isString } from '../util/object-utils.js'
 import { ColumnNode } from './column-node.js'
 import { IdentifierNode } from './identifier-node.js'
 import { OperationNode } from './operation-node.js'
 
 export interface UniqueConstraintNode extends OperationNode {
   readonly kind: 'UniqueConstraintNode'
-  readonly columns: ReadonlyArray<ColumnNode>
+  readonly columns: ReadonlyArray<OperationNode>
   readonly name?: IdentifierNode
   readonly nullsNotDistinct?: boolean
   readonly deferrable?: boolean
@@ -18,21 +19,56 @@ export type UniqueConstraintNodeProps = Omit<
 >
 
 /**
+ * TODO: remove this interface once support for `string[]` is removed.
+ *
  * @internal
  */
-export const UniqueConstraintNode = freeze({
+interface UniqueConstraintNodeFactory {
+  is(node: OperationNode): node is UniqueConstraintNode
+  create(
+    columns: OperationNode[],
+    constraintName?: string,
+    nullsNotDistinct?: boolean,
+  ): UniqueConstraintNode
+  /**
+   * @deprecated pass `ColumnNode[]` instead of strings.
+   */
+  create(
+    columns: string[],
+    constraintName?: string,
+    nullsNotDistinct?: boolean,
+  ): UniqueConstraintNode
+  cloneWith(
+    node: UniqueConstraintNode,
+    props: UniqueConstraintNodeProps,
+  ): UniqueConstraintNode
+}
+
+/**
+ * @internal
+ */
+export const UniqueConstraintNode: UniqueConstraintNodeFactory = freeze({
   is(node: OperationNode): node is UniqueConstraintNode {
     return node.kind === 'UniqueConstraintNode'
   },
 
   create(
-    columns: string[],
+    columns: string[] | OperationNode[],
     constraintName?: string,
     nullsNotDistinct?: boolean,
   ): UniqueConstraintNode {
+    // TODO: remove this block when support for `string[]` is removed.
+    if (isString(columns.at(0))) {
+      logOnce(
+        '`UniqueConstraintNode.create(columns: string[], ...)` is deprecated - pass `ColumnNode[]` instead.',
+      )
+
+      columns = (columns as string[]).map(ColumnNode.create)
+    }
+
     return freeze({
       kind: 'UniqueConstraintNode',
-      columns: freeze(columns.map(ColumnNode.create)),
+      columns: freeze(columns) as OperationNode[],
       name: constraintName ? IdentifierNode.create(constraintName) : undefined,
       nullsNotDistinct,
     })
@@ -42,9 +78,6 @@ export const UniqueConstraintNode = freeze({
     node: UniqueConstraintNode,
     props: UniqueConstraintNodeProps,
   ): UniqueConstraintNode {
-    return freeze({
-      ...node,
-      ...props,
-    })
+    return freeze({ ...node, ...props })
   },
 })
