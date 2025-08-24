99562823b166efb2e5daf7fbbaa19d039771544d
Igal Klebanov <igalklebanov@gmail.com>
2025-08-25 00:39:43 +0300

    feat(docs): applying google fonts optimization. (#1566)

--- site/docusaurus.config.ts
+++ site/docusaurus.config.ts
@@ -174,6 +174,32 @@ export default {
       ],
       style: 'dark',
     },
+    headTags: [
+      {
+        attributes: {
+          href: 'https://fonts.googleapis.com',
+          rel: 'preconnect',
+        },
+        tagName: 'link',
+      },
+      {
+        attributes: {
+          crossOrigin: 'anonymous',
+          href: 'https://fonts.gstatic.com',
+          rel: 'preconnect',
+        },
+        tagName: 'link',
+      },
+      {
+        attributes: {
+          as: 'style',
+          onLoad: "this.onload=null;this.rel='stylesheet'",
+          href: 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap',
+          rel: 'preload',
+        },
+        tagName: 'link',
+      },
+    ],
     mermaid: {
       options: {
         sequence: {
