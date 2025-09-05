2801d6be05a629701f46a6d30afceeb5f2d04f4d
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-09-05 14:17:05 +0700

    perf: optimize `randomString` utility function
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- src/util/random-string.ts
+++ src/util/random-string.ts
@@ -1,78 +1,10 @@
-const CHARS = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z',
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z',
-  '0',
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
-]
-
 export function randomString(length: number): string {
-  let chars = ''
-
-  for (let i = 0; i < length; ++i) {
-    chars += randomChar()
-  }
-
-  return chars
-}
-
-function randomChar() {
-  return CHARS[~~(Math.random() * CHARS.length)]
+  return String.fromCharCode(
+    ...Array.from({ length }, () => {
+      const n = Math.floor(Math.random() * 62)
+      if (n < 26) return 65 + n             // 'A'-'Z'
+      if (n < 52) return 97 + (n - 26)      // 'a'-'z'
+      return 48 + (n - 52)                  // '0'-'9'
+    })
+  )
 }
