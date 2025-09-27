8609862e947c3ee2ac1cb9562e8f45c63d82e531
Alexander Antia <34405368+aantia@users.noreply.github.com>
2025-09-28 00:53:31 +0100

    Support dropping multiple types with schema.dropType(), cascade. (#1516)
    Co-authored-by: Igal Klebanov <igalklebanov@gmail.com>

--- src/operation-node/drop-type-node.ts
+++ src/operation-node/drop-type-node.ts
@@ -2,12 +2,17 @@ import { freeze } from '../util/object-utils.js'
 import type { OperationNode } from './operation-node.js'
 import type { SchemableIdentifierNode } from './schemable-identifier-node.js'
 
-export type DropTypeNodeParams = Omit<Partial<DropTypeNode>, 'kind' | 'name'>
+export type DropTypeNodeParams = Omit<
+  Partial<DropTypeNode>,
+  'kind' | 'name' | 'additionalNames'
+>
 
 export interface DropTypeNode extends OperationNode {
   readonly kind: 'DropTypeNode'
   readonly name: SchemableIdentifierNode
+  readonly additionalNames?: SchemableIdentifierNode[]
   readonly ifExists?: boolean
+  readonly cascade?: boolean
 }
 
 type DropTypeNodeFactory = Readonly<{
@@ -27,10 +32,17 @@ export const DropTypeNode: DropTypeNodeFactory = freeze<DropTypeNodeFactory>({
     return node.kind === 'DropTypeNode'
   },
 
-  create(name) {
+  create(
+    names: SchemableIdentifierNode | SchemableIdentifierNode[],
+  ): DropTypeNode {
+    if (!Array.isArray(names)) {
+      names = [names]
+    }
+
     return freeze({
       kind: 'DropTypeNode',
-      name,
+      name: names[0],
+      additionalNames: names.slice(1),
     })
   },
 
