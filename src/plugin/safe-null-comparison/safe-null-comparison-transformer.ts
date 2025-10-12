28625d332a83e98ab44980bf4fd387a1b5866824
Rafael Almeida <rafaelalmeidatk@gmail.com>
2025-10-12 08:04:38 -0300

    feat: add `SafeNullComparisonPlugin` plugin (#1338)
    Co-authored-by: Igal Klebanov <igalklebanov@gmail.com>

---
+++ src/plugin/safe-null-comparison/safe-null-comparison-transformer.ts
@@ -0,0 +1,33 @@
+import { BinaryOperationNode } from '../../operation-node/binary-operation-node.js'
+import { OperationNodeTransformer } from '../../operation-node/operation-node-transformer.js'
+import { OperatorNode } from '../../operation-node/operator-node.js'
+import { ValueNode } from '../../operation-node/value-node.js'
+
+export class SafeNullComparisonTransformer extends OperationNodeTransformer {
+  protected transformBinaryOperation(
+    node: BinaryOperationNode,
+  ): BinaryOperationNode {
+    const { operator, leftOperand, rightOperand } =
+      super.transformBinaryOperation(node)
+
+    if (
+      !ValueNode.is(rightOperand) ||
+      rightOperand.value !== null ||
+      !OperatorNode.is(operator)
+    ) {
+      return node
+    }
+
+    const op = operator.operator
+    
+    if (op !== '=' && op !== '!=' && op !== '<>') {
+      return node
+    }
+
+    return BinaryOperationNode.create(
+      leftOperand,
+      OperatorNode.create(op === '=' ? 'is' : 'is not'),
+      rightOperand,
+    )
+  }
+}
