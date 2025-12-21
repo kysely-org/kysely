9b04c835f3a4228e911eb5965f445aa61cd51e26
Igal Klebanov <igalklebanov@gmail.com>
2025-12-21 11:02:17 +0100

    feat: add more control through configuration @ ParseJSONResultsPlugin. (#1453)

--- test/node/src/parse-json-results-plugin.test.ts
+++ test/node/src/parse-json-results-plugin.test.ts
@@ -1,5 +1,7 @@
 import { ParseJSONResultsPlugin } from '../../..'
 import { createQueryId } from '../../../dist/cjs/util/query-id.js'
+import { expect } from './test-setup'
+import merge from 'prototype-pollution-vulnerable-lodash.merge-dont-upgrade'
 
 describe('ParseJSONResultsPlugin', () => {
   describe("when `objectStrategy` is 'create'", () => {
@@ -23,5 +25,48 @@ describe('ParseJSONResultsPlugin', () => {
         },
       })
     })
+  })
+
+  it('should omit dangerious keys when JSON parsing, denying prototype pollution downstream', async () => {
+    const plugin = new ParseJSONResultsPlugin({ objectStrategy: 'create' })
+
+    const maliciousRow = {
+      id: 1,
+      __proto__: JSON.stringify({ isAdmin: true }),
+      joe: JSON.stringify({
+        age: 30,
+        __proto__: { isAdmin: true },
+        constructor: JSON.stringify({
+          prototype: { isAdmin: true },
+          true: false,
+        }),
+        joe: JSON.stringify({
+          __proto__: { isAdmin: true },
+          true: false,
+        }),
+      }),
+      constructor: JSON.stringify({
+        prototype: { isAdmin: true },
+        true: false,
+        __proto__: { isAdmin: true },
+      }),
+      prototype: JSON.stringify({
+        isAdmin: true,
+        __proto__: { isAdmin: true },
+      }),
+    }
+
+    const {
+      rows: [rowParsedByPlugin],
+    } = await plugin.transformResult({
+      queryId: createQueryId(),
+      result: {
+        rows: [maliciousRow],
+      },
+    })
+
+    const mergedWithRowParsedByPlugin = merge({}, rowParsedByPlugin)
+
+    expect((mergedWithRowParsedByPlugin as any).isAdmin).to.be.undefined
   })
 })
