79323de1c480d9dbfd3e6504ecf1d97cec39b86c
igalklebanov <igalklebanov@gmail.com>
2026-01-30 02:57:30 +0200

    verbatim.

--- test/typings/test-d/minimal-type.test-d.ts
+++ test/typings/test-d/minimal-type.test-d.ts
@@ -1,21 +1,26 @@
-
-import { Database } from '../shared'
-import { MinimalSelectQueryBuilder, Kysely, AnyColumn, SelectQueryBuilder } from '..'
+import type { Database } from '../shared'
+import type {
+  MinimalSelectQueryBuilder,
+  Kysely,
+  AnyColumn,
+  SelectQueryBuilder,
+} from '..'
 import { expectType } from 'tsd'
 
 async function testMinimalType(db: Kysely<Database>) {
-    const stm = db.selectFrom("book")
-    const stm2 = addSelect(stm, ["id", "name"])
-    expectType<SelectQueryBuilder<Database, "book", { id: number; name: string; }>>(stm2)
-    // @ts-expect-error
-    addSelect(stm, ["name2"])
-
+  const stm = db.selectFrom('book')
+  const stm2 = addSelect(stm, ['id', 'name'])
+  expectType<
+    SelectQueryBuilder<Database, 'book', { id: number; name: string }>
+  >(stm2)
+  // @ts-expect-error
+  addSelect(stm, ['name2'])
 }
 
 export function addSelect<DB, T extends keyof DB, O>(
-    inQB: MinimalSelectQueryBuilder<DB, T, O>,
-    fields: AnyColumn<DB, T>[]
+  inQB: MinimalSelectQueryBuilder<DB, T, O>,
+  fields: AnyColumn<DB, T>[],
 ) {
-    const qb = inQB as SelectQueryBuilder<DB, T, O>
-    return qb.select(fields)
+  const qb = inQB as SelectQueryBuilder<DB, T, O>
+  return qb.select(fields)
 }
