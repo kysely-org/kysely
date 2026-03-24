28989509605b0c0a1f0c2ba4e99f3744c5a945a7
Igal Klebanov <igalklebanov@gmail.com>
2026-03-24 17:32:06 +0200

    ...

--- src/parser/on-modify-action-parser.ts
+++ src/parser/on-modify-action-parser.ts
@@ -1,13 +1,13 @@
 import {
+  isOnModifyForeignAction,
   type OnModifyForeignAction,
-  ON_MODIFY_FOREIGN_ACTIONS_RECORD,
 } from '../operation-node/references-node.js'
 
 export function parseOnModifyForeignAction(
   action: string,
 ): OnModifyForeignAction {
-  if (ON_MODIFY_FOREIGN_ACTIONS_RECORD[action as never]) {
-    return action as never
+  if (isOnModifyForeignAction(action)) {
+    return action
   }
 
   throw new Error(`invalid OnModifyForeignAction ${action}`)
