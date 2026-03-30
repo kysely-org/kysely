59e3b5af8225353608dbe4242869d96800866f31
Igal Klebanov <igalklebanov@gmail.com>
2026-03-30 05:43:01 +0300

    refactor: bump minimum TypeScript version to 5.4. (#1773)

--- test/outdated-ts/outdated-ts.test.ts
+++ test/outdated-ts/outdated-ts.test.ts
@@ -2,7 +2,7 @@ import { Kysely, RawBuilder, sql } from 'kysely'
 import { KyselyTypeError } from '../../src/util/type-error'
 
 function expectOutdatedTSError(
-  _: KyselyTypeError<'The installed TypeScript version is outdated and cannot guarantee type-safety with Kysely. Please upgrade to version 5.3 or newer.'>,
+  _: KyselyTypeError<'The installed TypeScript version is outdated and cannot guarantee type-safety with Kysely. Please upgrade to version 5.4 or newer.'>,
 ): void {}
 
 expectOutdatedTSError(Kysely)
