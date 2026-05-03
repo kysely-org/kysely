ae95b73a09720414c209c5d9ba90fd928c057b6e
Igal Klebanov <igalklebanov@gmail.com>
2026-05-03 03:48:54 +0300

    ...

--- test/node/src/sql-injection.test.ts
+++ test/node/src/sql-injection.test.ts
@@ -6,6 +6,8 @@ import {
   DIALECTS,
   initJSONTest,
   insertDefaultJSONDataSet,
+  NOT_SUPPORTED,
+  testSql,
   type JSONTestContext,
 } from './test-setup.js'
 
@@ -72,7 +74,7 @@ for (const dialect of DIALECTS) {
       await assertDidNotDropTable(ctx, 'person')
     })
 
-    if (dialect === 'mysql') {
+    if (dialect === 'mysql' || dialect === 'sqlite') {
       it('should not allow SQL injection in $.key JSON paths', async () => {
         const injection =
           `first' as ${identifierWrapper}first${identifierWrapper} from ${identifierWrapper}people${identifierWrapper}; drop table ${identifierWrapper}person${identifierWrapper} -- ` as never
@@ -90,9 +92,18 @@ for (const dialect of DIALECTS) {
           .selectFrom('people')
           .select((eb) => eb.ref('data', '->$').key(injection).as('first'))
 
-        expect(query.compile().sql).to.equal(
-          `with ${identifierWrapper}people${identifierWrapper} as (select json_object('first', first_name) as ${identifierWrapper}data${identifierWrapper} from ${identifierWrapper}person${identifierWrapper}) select ${identifierWrapper}data${identifierWrapper}->'$."first'' as ${identifierWrapper}first${identifierWrapper} from ${identifierWrapper}people${identifierWrapper}; drop table ${identifierWrapper}person${identifierWrapper} -- "' as ${identifierWrapper}first${identifierWrapper} from ${identifierWrapper}people${identifierWrapper}`,
-        )
+        testSql(query, dialect, {
+          postgres: NOT_SUPPORTED,
+          mysql: {
+            sql: `with \`people\` as (select json_object('first', first_name) as \`data\` from \`person\`) select \`data\`->'$."first'' as \`first\` from \`people\`; drop table \`person\` -- "' as \`first\` from \`people\``,
+            parameters: [],
+          },
+          mssql: NOT_SUPPORTED,
+          sqlite: {
+            sql: `with "people" as (select json_object('first', first_name) as "data" from "person") select "data"->'$."first'' as \\"first\\" from \\"people\\"; drop table \\"person\\" -- "' as "first" from "people"`,
+            parameters: [],
+          },
+        })
         await ctx.db.executeQuery(query)
         await assertDidNotDropTable(ctx, 'person')
       })
@@ -114,9 +125,18 @@ for (const dialect of DIALECTS) {
           .selectFrom('people')
           .select((eb) => eb.ref('data', '->$').key(injection).as('first'))
 
-        expect(query.compile().sql).to.equal(
-          `with ${identifierWrapper}people${identifierWrapper} as (select json_object('first', first_name) as ${identifierWrapper}data${identifierWrapper} from ${identifierWrapper}person${identifierWrapper}) select ${identifierWrapper}data${identifierWrapper}->'$."first\\\\\\\\'' as ${identifierWrapper}first${identifierWrapper} from ${identifierWrapper}people${identifierWrapper}; drop table ${identifierWrapper}pet${identifierWrapper} -- "' as ${identifierWrapper}first${identifierWrapper} from ${identifierWrapper}people${identifierWrapper}`,
-        )
+        testSql(query, dialect, {
+          postgres: NOT_SUPPORTED,
+          mysql: {
+            sql: `with \`people\` as (select json_object('first', first_name) as \`data\` from \`person\`) select \`data\`->'$."first\\\\\\\\'' as \`first\` from \`people\`; drop table \`pet\` -- "' as \`first\` from \`people\``,
+            parameters: [],
+          },
+          mssql: NOT_SUPPORTED,
+          sqlite: {
+            sql: `with "people" as (select json_object('first', first_name) as "data" from "person") select "data"->'$."first\\\\'' as \\"first\\" from \\"people\\"; drop table \\"pet\\" -- "' as "first" from "people"`,
+            parameters: [],
+          },
+        })
         await ctx.db.executeQuery(query)
         await assertDidNotDropTable(ctx, 'pet')
       })
@@ -129,9 +149,18 @@ for (const dialect of DIALECTS) {
           .where('first_name', '=', sql.lit(injection))
           .selectAll()
 
-        expect(query.compile().sql).to.equal(
-          `select * from ${identifierWrapper}person${identifierWrapper} where ${identifierWrapper}first_name${identifierWrapper} = '\\\\''; drop table ${identifierWrapper}person${identifierWrapper}; -- '`,
-        )
+        testSql(query, dialect, {
+          postgres: NOT_SUPPORTED,
+          mysql: {
+            sql: `select * from \`person\` where \`first_name\` = '\\\\''; drop table \`person\`; -- '`,
+            parameters: [],
+          },
+          mssql: NOT_SUPPORTED,
+          sqlite: {
+            sql: `select * from "person" where "first_name" = '\\''; drop table "person"; -- '`,
+            parameters: [],
+          },
+        })
         await ctx.db.executeQuery(query)
         await assertDidNotDropTable(ctx, 'person')
       })
@@ -146,9 +175,18 @@ for (const dialect of DIALECTS) {
             .as('exfiltrated'),
         )
 
-        expect(query.compile().sql).to.equal(
-          `select ${identifierWrapper}profile${identifierWrapper}->'$."${exfiltration}"' as ${identifierWrapper}exfiltrated${identifierWrapper} from ${identifierWrapper}person_metadata${identifierWrapper}`,
-        )
+        testSql(query, dialect, {
+          postgres: NOT_SUPPORTED,
+          mysql: {
+            sql: `select \`profile\`->'$."${exfiltration}"' as \`exfiltrated\` from \`person_metadata\``,
+            parameters: [],
+          },
+          mssql: NOT_SUPPORTED,
+          sqlite: {
+            sql: `select "profile"->'$."${exfiltration}"' as "exfiltrated" from "person_metadata"`,
+            parameters: [],
+          },
+        })
 
         const result = await query.executeTakeFirstOrThrow()
 
@@ -165,9 +203,18 @@ for (const dialect of DIALECTS) {
             .as('exfiltrated'),
         )
 
-        expect(query.compile().sql).to.equal(
-          `select ${identifierWrapper}profile${identifierWrapper}->'$."auth\\\\".\\\\"login_count"' as ${identifierWrapper}exfiltrated${identifierWrapper} from ${identifierWrapper}person_metadata${identifierWrapper}`,
-        )
+        testSql(query, dialect, {
+          postgres: NOT_SUPPORTED,
+          mysql: {
+            sql: `select \`profile\`->'$."auth\\\\".\\\\"login_count"' as \`exfiltrated\` from \`person_metadata\``,
+            parameters: [],
+          },
+          mssql: NOT_SUPPORTED,
+          sqlite: {
+            sql: `select "profile"->'$."auth\\".\\"login_count"' as "exfiltrated" from "person_metadata"`,
+            parameters: [],
+          },
+        })
 
         const result = await query.executeTakeFirstOrThrow()
 
@@ -184,16 +231,25 @@ for (const dialect of DIALECTS) {
             .as('exfiltrated'),
         )
 
-        expect(query.compile().sql).to.equal(
-          `select ${identifierWrapper}profile${identifierWrapper}->'$."tags[0]"' as ${identifierWrapper}exfiltrated${identifierWrapper} from ${identifierWrapper}person_metadata${identifierWrapper}`,
-        )
+        testSql(query, dialect, {
+          postgres: NOT_SUPPORTED,
+          mysql: {
+            sql: `select \`profile\`->'$."tags[0]"' as \`exfiltrated\` from \`person_metadata\``,
+            parameters: [],
+          },
+          mssql: NOT_SUPPORTED,
+          sqlite: {
+            sql: `select "profile"->'$."tags[0]"' as "exfiltrated" from "person_metadata"`,
+            parameters: [],
+          },
+        })
 
         const result = await query.executeTakeFirstOrThrow()
 
         expect(result.exfiltrated).to.be.null
       })
 
-      it('should not allow exfiltration via $.* in $.key JSON paths', async () => {
+      it('should not allow exfiltration via wildcard in $.key JSON paths', async () => {
         const exfiltration = '*'
 
         const query = ctx.db.selectFrom('person_metadata').select((eb) =>
@@ -203,16 +259,25 @@ for (const dialect of DIALECTS) {
             .as('exfiltrated'),
         )
 
-        expect(query.compile().sql).to.equal(
-          `select ${identifierWrapper}profile${identifierWrapper}->'$."*"' as ${identifierWrapper}exfiltrated${identifierWrapper} from ${identifierWrapper}person_metadata${identifierWrapper}`,
-        )
+        testSql(query, dialect, {
+          postgres: NOT_SUPPORTED,
+          mysql: {
+            sql: `select \`profile\`->'$."*"' as \`exfiltrated\` from \`person_metadata\``,
+            parameters: [],
+          },
+          mssql: NOT_SUPPORTED,
+          sqlite: {
+            sql: `select "profile"->'$."*"' as "exfiltrated" from "person_metadata"`,
+            parameters: [],
+          },
+        })
 
         const result = await query.executeTakeFirstOrThrow()
 
         expect(result.exfiltrated).to.be.null
       })
 
-      it('should not allow exfiltration via $[*] in $.key JSON paths', async () => {
+      it('should not allow exfiltration via wildcard in $.at JSON paths', async () => {
         const exfiltration = '*'
 
         expect(() =>
