aef4db5d381bebc5136099ef1e3ca5bba12fc0c2
Théo LUDWIG <contact@theoludwig.fr>
2026-05-22 16:42:17 +0200

    docs: kysely-typegen

--- site/src/components/SectionFeatures/index.tsx
+++ site/src/components/SectionFeatures/index.tsx
@@ -15,7 +15,7 @@ const FeatureList: FeatureItem[] = [
         Kysely's state-of-the-art, type-safe API provides precise result types
         and catches errors within queries at compile-time, giving
         high-performing teams the confidence to ship at greater velocity. Use
-        `kysely-codegen` to make the database the source of types.
+        `kysely-codegen` or `kysely-typegen` to make the database the source of types.
       </>
     ),
   },
