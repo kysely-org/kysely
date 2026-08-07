edc15b0a8f747c8d5039315d07e247c5a5c68c19
Igal Klebanov <igalklebanov@gmail.com>
2026-08-07 15:19:47 +0300

    ...

--- site/src/theme/DocCategoryGeneratedIndexPage/index.tsx
+++ site/src/theme/DocCategoryGeneratedIndexPage/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import DocCategoryGeneratedIndexPage from '@theme-original/DocCategoryGeneratedIndexPage'
-import type {Props} from '@theme/DocCategoryGeneratedIndexPage'
+import type { Props } from '@theme/DocCategoryGeneratedIndexPage'
 
 // Generated category pages (/docs/category/*) are navigation stubs; their
 // children are indexed individually, so keep the stubs out of the search
