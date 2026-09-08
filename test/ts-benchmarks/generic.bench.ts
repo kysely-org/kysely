b982b2c44437ebb246f1a8c714672a762b449715
Igal Klebanov <igalklebanov@gmail.com>
2026-09-08 22:30:49 +0300

    ...

--- test/ts-benchmarks/generic.bench.ts
+++ test/ts-benchmarks/generic.bench.ts
@@ -158,7 +158,7 @@ bench('WheneableMergeQueryBuilder assignable to a narrower one', () => {
 
 bench('Transaction assignable to Kysely', () => {
   return acceptsKysely(transaction)
-}).types([160079, 'instantiations'])
+}).types([109366, 'instantiations'])
 
 bench('MergeQueryBuilder assignable to narrower MergeQueryBuilder', () => {
   return acceptsNarrowMergeInto(wideMergeInto)
