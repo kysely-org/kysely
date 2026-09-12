c7ff4ce2c606738398175014d2b56073f34d956d
Igal Klebanov <igalklebanov@gmail.com>
2026-09-12 13:38:20 +0300

    docs: explain function module declaration merging

--- src/query-builder/function-module.ts
+++ src/query-builder/function-module.ts
@@ -31,6 +31,7 @@ import { isString } from '../util/object-utils.js'
 import { parseTable } from '../parser/table-parser.js'
 import type { Selectable, SelectType } from '../util/column-type.js'
 
+// Merge a class for the protected marker with an interface for call signatures.
 // Declare the schema before the callable interface so it is compared first.
 // This avoids lost variance reliability in older TypeScript versions.
 declare class FunctionModule<DB, TB extends keyof DB> {
@@ -40,6 +41,8 @@ declare class FunctionModule<DB, TB extends keyof DB> {
     : { [T in TB]: Selectable<DB[T]> }
 }
 
+// Merged declarations must both be exported or both local. Keep them local and
+// export only the type so we don't expose a class that doesn't exist at runtime.
 export type { FunctionModule }
 
 /**
